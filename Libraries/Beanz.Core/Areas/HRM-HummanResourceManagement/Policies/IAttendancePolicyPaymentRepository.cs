cd915ba4bcf9a19ac73ae67deb8c50b4e6dced71
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:53:07 +0300

    Lovable: scaffold IAttendancePolicyPaymentRepository.cs for AttendancePolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAttendancePolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAttendancePolicyPaymentRepository
+    {
+        Task<List<AttendancePolicyPaymentDTO>> GetAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAttendancePolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<AttendancePolicyPaymentViewModel> GetInfoAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<AttendancePolicyPaymentViewModel> PrintAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAttendancePolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
