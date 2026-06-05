0ee3abeb31f39a005d9df9ca3e01ad1c49de1dd6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:13:17 +0300

    Lovable: scaffold ITrainingPolicyEligibleRepository.cs for TrainingPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ITrainingPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ITrainingPolicyEligibleRepository
+    {
+        Task<List<TrainingPolicyEligibleDTO>> GetTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTrainingPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<TrainingPolicyEligibleViewModel> GetInfoTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<TrainingPolicyEligibleViewModel> PrintTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTrainingPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
