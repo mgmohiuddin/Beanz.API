9ade4e930a9505f4e1f3931aecad5f672aafd3e5
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:44:32 +0300

    Lovable: scaffold IAdvanceSalaryPolicyPaymentRepository.cs for AdvanceSalaryPolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAdvanceSalaryPolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAdvanceSalaryPolicyPaymentRepository
+    {
+        Task<List<AdvanceSalaryPolicyPaymentDTO>> GetAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<AdvanceSalaryPolicyPaymentViewModel> GetInfoAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<AdvanceSalaryPolicyPaymentViewModel> PrintAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAdvanceSalaryPolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
