27446f8259ff3056459c68e9269d6b4f81a7146e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:12:42 +0300

    Lovable: scaffold IEmployeeDocumentRepository.cs for EmployeeDocuments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeDocumentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeDocumentRepository
+    {
+        Task<List<EmployeeDocumentDTO>> GetEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeDocumentsAsync(BeanzCommonDTO lookup);
+        Task<EmployeeDocumentViewModel> GetInfoEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<EmployeeDocumentViewModel> PrintEmployeeDocumentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeDocumentsAsync(BeanzCommonDTO common);
+    }
+}
