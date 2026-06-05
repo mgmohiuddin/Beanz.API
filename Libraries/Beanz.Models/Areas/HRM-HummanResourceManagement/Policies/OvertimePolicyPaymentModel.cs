bf099c7e30a22fb5a4d1a61a6a08a618f82fd74f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:08:40 +0300

    Lovable: scaffold OvertimePolicyPaymentModel.cs for OvertimePolicyPayments

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Policies/OvertimePolicyPaymentModel.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Policies.Objects
+{
+    // Entity model generated from table structure
+    public class OvertimePolicyPayment
+    {
+        public int OvertimePolicyPaymentID { get; set; }
+        public int OverTimePolicyID { get; set; }
+        public int AllowanceID { get; set; }
+        public int AllowanceTypeID { get; set; }
+        public string OvertimePolicyPaymentCode { get; set; }
+        public string OvertimePolicyPaymentName { get; set; }
+        public string OvertimePolicyPaymentAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
