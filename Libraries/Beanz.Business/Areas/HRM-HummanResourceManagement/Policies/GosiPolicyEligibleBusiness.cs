1f076d02dcc8b92dcf5844992078d97af7eb504c
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:57:52 +0300

    Lovable: scaffold GosiPolicyEligibleBusiness.cs for GosiPolicyEligibles

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/GosiPolicyEligibleBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class GosiPolicyEligibleBusiness
+    {
+        private readonly IGosiPolicyEligibleRepository _repository;
+
+        public GosiPolicyEligibleBusiness(IGosiPolicyEligibleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<GosiPolicyEligibleDTO>> GetGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetGosiPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> SetGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.SetGosiPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> PostGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.PostGosiPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> DelGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.DelGosiPolicyEligiblesAsync(common);
+
+        public Task<GosiPolicyEligibleViewModel> GetInfoGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoGosiPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveGosiPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.ApproveGosiPolicyEligiblesAsync(common);
+    }
+}
