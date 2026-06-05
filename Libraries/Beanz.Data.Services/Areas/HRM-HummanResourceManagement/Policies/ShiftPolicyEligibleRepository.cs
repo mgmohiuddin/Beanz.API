2ad9b0a4b06cde32ff35ac3ed5b99ecf475b26f6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:11:40 +0300

    Lovable: scaffold ShiftPolicyEligibleRepository.cs for ShiftPolicyEligibles

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Policies/ShiftPolicyEligibleRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Policies
+{
+    public class ShiftPolicyEligibleRepository : IShiftPolicyEligibleRepository
+    {
+        private readonly IMapper _mapper;
+
+        public ShiftPolicyEligibleRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<ShiftPolicyEligibleDTO>> GetShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetPOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<ShiftPolicyEligibleDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetPOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostPOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelPOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpShiftPolicyEligiblesAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupPOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<ShiftPolicyEligibleViewModel> GetInfoShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoPOL_ShiftPolicyEligibles";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _shiftPolicyEligibleDTOList = new List<ShiftPolicyEligibleDTO>();
+            bool _isShiftPolicyEligiblesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var shiftPolicyEligibleData = objTemp.Where(c => c.TableName == "POL_ShiftPolicyEligibles");
+                        if (shiftPolicyEligibleData.Count() > 0 && !_isShiftPolicyEligiblesConsumed)
+                        {
+                            _shiftPolicyEligibleDTOList = _mapper.Map<List<ShiftPolicyEligibleDTO>>(shiftPolicyEligibleData);
+                            _isShiftPolicyEligiblesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _shiftPolicyEligibleDTO = _shiftPolicyEligibleDTOList.Where(x => x.ShiftPolicyEligibleID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new ShiftPolicyEligibleViewModel
+                {
+                    ShiftPolicyEligibles = _shiftPolicyEligibleDTOList,
+                    ShiftPolicyEligible = _shiftPolicyEligibleDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<ShiftPolicyEligibleViewModel> PrintShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintPOL_ShiftPolicyEligibles";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _shiftPolicyEligibleDTOList = new List<ShiftPolicyEligibleDTO>();
+            bool _isShiftPolicyEligiblesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var shiftPolicyEligibleData = objTemp.Where(c => c.TableName == "POL_ShiftPolicyEligibles");
+                        if (shiftPolicyEligibleData.Count() > 0 && !_isShiftPolicyEligiblesConsumed)
+                        {
+                            _shiftPolicyEligibleDTOList = _mapper.Map<List<ShiftPolicyEligibleDTO>>(shiftPolicyEligibleData);
+                            _isShiftPolicyEligiblesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _shiftPolicyEligibleDTO = _shiftPolicyEligibleDTOList.Where(x => x.ShiftPolicyEligibleID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new ShiftPolicyEligibleViewModel
+                {
+                    ShiftPolicyEligibles = _shiftPolicyEligibleDTOList,
+                    ShiftPolicyEligible = _shiftPolicyEligibleDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveShiftPolicyEligiblesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApprovePOL_ShiftPolicyEligibles";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ShiftPolicyEligibleID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
