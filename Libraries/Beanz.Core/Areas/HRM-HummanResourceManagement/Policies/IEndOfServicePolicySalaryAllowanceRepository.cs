f0d263f5cb79ea6c8c86799bb0b61f80aeb5f4d2
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:56:08 +0300

    Lovable: scaffold IEndOfServicePolicySalaryAllowanceRepository.cs for EndOfServicePolicySalaryAllowances

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IEndOfServicePolicySalaryAllowanceRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IEndOfServicePolicySalaryAllowanceRepository
+    {
+        Task<List<EndOfServicePolicySalaryAllowanceDTO>> GetEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO lookup);
+        Task<EndOfServicePolicySalaryAllowanceViewModel> GetInfoEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<EndOfServicePolicySalaryAllowanceViewModel> PrintEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEndOfServicePolicySalaryAllowancesAsync(BeanzCommonDTO common);
+    }
+}
