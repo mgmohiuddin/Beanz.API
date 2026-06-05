5a26e1e5f7c9540103be081a664d0a7de564085e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:16:30 +0300

    Lovable: scaffold EmployeeAllowanceDTO.cs for EmployeeAllowances

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Employees/EmployeeAllowanceDTO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeAllowanceDTO
+    {
+        public int EmployeeAllowanceID { get; set; }
+        public int AllowanceID { get; set; }
+        public int AllowanceTypeID { get; set; }
+        public string EmployeeAllowanceCode { get; set; }
+        public string EmployeeAllowanceName { get; set; }
+        public string EmployeeAllowanceAlias { get; set; }
+        public int EmployeeID { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class EmployeeAllowanceViewModel
+    {
+        public List<EmployeeAllowanceDTO> EmployeeAllowances { get; set; }
+        public EmployeeAllowanceDTO EmployeeAllowance { get; set; }
+    }
+}
