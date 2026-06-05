4a22e61651b7c5ac2aa926d3bd3fc0faebe3eae7
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:06:15 +0300

    Lovable: scaffold ILoanPolicyEligibleRepository.cs for LoanPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ILoanPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ILoanPolicyEligibleRepository
+    {
+        Task<List<LoanPolicyEligibleDTO>> GetLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpLoanPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<LoanPolicyEligibleViewModel> GetInfoLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<LoanPolicyEligibleViewModel> PrintLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveLoanPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
