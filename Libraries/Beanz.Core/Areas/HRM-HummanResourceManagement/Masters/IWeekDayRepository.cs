ae76c364898dc775c2101169b48d02f6eedddf1e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:40:15 +0300

    Lovable: scaffold IWeekDayRepository.cs for WeekDays

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IWeekDayRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IWeekDayRepository
+    {
+        Task<List<WeekDayDTO>> GetWeekDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetWeekDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostWeekDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelWeekDaysAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpWeekDaysAsync(BeanzCommonDTO lookup);
+        Task<WeekDayViewModel> GetInfoWeekDaysAsync(BeanzCommonDTO common);
+        Task<WeekDayViewModel> PrintWeekDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveWeekDaysAsync(BeanzCommonDTO common);
+    }
+}
