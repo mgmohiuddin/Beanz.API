a1ab9e3997b3a9e247151493d11ed43beb3fbc77
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:33:04 +0300

    Lovable: scaffold IHolidayTypeRepository.cs for HolidayTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IHolidayTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IHolidayTypeRepository
+    {
+        Task<List<HolidayTypeDTO>> GetHolidayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetHolidayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostHolidayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelHolidayTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpHolidayTypesAsync(BeanzCommonDTO lookup);
+        Task<HolidayTypeViewModel> GetInfoHolidayTypesAsync(BeanzCommonDTO common);
+        Task<HolidayTypeViewModel> PrintHolidayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveHolidayTypesAsync(BeanzCommonDTO common);
+    }
+}
