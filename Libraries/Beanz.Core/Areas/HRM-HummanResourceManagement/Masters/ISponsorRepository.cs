fa92027d783ea79bed99578a3793f93b69011b08
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:37:05 +0300

    Lovable: scaffold ISponsorRepository.cs for Sponsors

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/ISponsorRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface ISponsorRepository
+    {
+        Task<List<SponsorDTO>> GetSponsorsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetSponsorsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostSponsorsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelSponsorsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpSponsorsAsync(BeanzCommonDTO lookup);
+        Task<SponsorViewModel> GetInfoSponsorsAsync(BeanzCommonDTO common);
+        Task<SponsorViewModel> PrintSponsorsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveSponsorsAsync(BeanzCommonDTO common);
+    }
+}
