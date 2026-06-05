6709b88c1b8960dcd0719a6a031c1f219749d070
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:10:37 +0300

    Lovable: scaffold PayrollPolicyPaymentRepository.cs for PayrollPolicyPayments

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Policies/PayrollPolicyPaymentRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Policies
+{
+    public class PayrollPolicyPaymentRepository : IPayrollPolicyPaymentRepository
+    {
+        private readonly IMapper _mapper;
+
+        public PayrollPolicyPaymentRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<PayrollPolicyPaymentDTO>> GetPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetPOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<PayrollPolicyPaymentDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetPOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostPOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelPOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpPayrollPolicyPaymentsAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupPOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<PayrollPolicyPaymentViewModel> GetInfoPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoPOL_PayrollPolicyPayments";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _payrollPolicyPaymentDTOList = new List<PayrollPolicyPaymentDTO>();
+            bool _isPayrollPolicyPaymentsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var payrollPolicyPaymentData = objTemp.Where(c => c.TableName == "POL_PayrollPolicyPayments");
+                        if (payrollPolicyPaymentData.Count() > 0 && !_isPayrollPolicyPaymentsConsumed)
+                        {
+                            _payrollPolicyPaymentDTOList = _mapper.Map<List<PayrollPolicyPaymentDTO>>(payrollPolicyPaymentData);
+                            _isPayrollPolicyPaymentsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _payrollPolicyPaymentDTO = _payrollPolicyPaymentDTOList.Where(x => x.PayrollPolicyPaymentID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new PayrollPolicyPaymentViewModel
+                {
+                    PayrollPolicyPayments = _payrollPolicyPaymentDTOList,
+                    PayrollPolicyPayment = _payrollPolicyPaymentDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<PayrollPolicyPaymentViewModel> PrintPayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintPOL_PayrollPolicyPayments";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _payrollPolicyPaymentDTOList = new List<PayrollPolicyPaymentDTO>();
+            bool _isPayrollPolicyPaymentsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var payrollPolicyPaymentData = objTemp.Where(c => c.TableName == "POL_PayrollPolicyPayments");
+                        if (payrollPolicyPaymentData.Count() > 0 && !_isPayrollPolicyPaymentsConsumed)
+                        {
+                            _payrollPolicyPaymentDTOList = _mapper.Map<List<PayrollPolicyPaymentDTO>>(payrollPolicyPaymentData);
+                            _isPayrollPolicyPaymentsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _payrollPolicyPaymentDTO = _payrollPolicyPaymentDTOList.Where(x => x.PayrollPolicyPaymentID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new PayrollPolicyPaymentViewModel
+                {
+                    PayrollPolicyPayments = _payrollPolicyPaymentDTOList,
+                    PayrollPolicyPayment = _payrollPolicyPaymentDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApprovePayrollPolicyPaymentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApprovePOL_PayrollPolicyPayments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@PayrollPolicyPaymentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
