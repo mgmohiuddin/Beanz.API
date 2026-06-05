cf5f9c5a06e1f66efecaea6316172b5719916ebf
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:10:13 +0300

    Lovable: scaffold IPayrollPolicyEligibleRepository.cs for PayrollPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IPayrollPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IPayrollPolicyEligibleRepository
+    {
+        Task<List<PayrollPolicyEligibleDTO>> GetPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPayrollPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<PayrollPolicyEligibleViewModel> GetInfoPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<PayrollPolicyEligibleViewModel> PrintPayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePayrollPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
