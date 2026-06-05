ca87f6942c3404492f09ce2ab85eb72ae49f282c
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:47:00 +0300

    Lovable: scaffold AdvanceSalaryPolicyPaymentDTO.cs for AdvanceSalaryPolicyPayments

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/AdvanceSalaryPolicyPaymentDTO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class AdvanceSalaryPolicyPaymentDTO
+    {
+        public int AdvanceSalaryPolicyPaymentID { get; set; }
+        public int AdvanceSalaryPolicyID { get; set; }
+        public int AllowanceID { get; set; }
+        public int AllowanceTypeID { get; set; }
+        public string AdvanceSalaryPolicyPaymentCode { get; set; }
+        public string AdvanceSalaryPolicyPaymentName { get; set; }
+        public string AdvanceSalaryPolicyPaymentAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class AdvanceSalaryPolicyPaymentViewModel
+    {
+        public List<AdvanceSalaryPolicyPaymentDTO> AdvanceSalaryPolicyPayments { get; set; }
+        public AdvanceSalaryPolicyPaymentDTO AdvanceSalaryPolicyPayment { get; set; }
+    }
+}
