65616cc36617c79ac1ea9c99d0eff9c7fcc6e1eb
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:11:20 +0300

    Lovable: scaffold IShiftPolicieRepository.cs for ShiftPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IShiftPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IShiftPolicieRepository
+    {
+        Task<List<ShiftPolicieDTO>> GetShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpShiftPoliciesAsync(BeanzCommonDTO lookup);
+        Task<ShiftPolicieViewModel> GetInfoShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<ShiftPolicieViewModel> PrintShiftPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveShiftPoliciesAsync(BeanzCommonDTO common);
+    }
+}
