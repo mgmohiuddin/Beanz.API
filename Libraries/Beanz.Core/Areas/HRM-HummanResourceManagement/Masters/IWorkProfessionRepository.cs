2fd157e9c0c8e3bc9380387ebceb4d298009fd30
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:41:32 +0300

    Lovable: scaffold IWorkProfessionRepository.cs for WorkProfessions

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IWorkProfessionRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IWorkProfessionRepository
+    {
+        Task<List<WorkProfessionDTO>> GetWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpWorkProfessionsAsync(BeanzCommonDTO lookup);
+        Task<WorkProfessionViewModel> GetInfoWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<WorkProfessionViewModel> PrintWorkProfessionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveWorkProfessionsAsync(BeanzCommonDTO common);
+    }
+}
