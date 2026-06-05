4ad8f751c15a6e53b853b161592cef92fc2fcf55
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:19:56 +0300

    Lovable: scaffold LeaveOpeningBalanceDTO.cs for LeaveOpeningBalances

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Transactions/LeaveOpeningBalanceDTO.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Transactions
+{
+    public class LeaveOpeningBalanceDTO
+    {
+        public int LeaveOpeningBalanceID { get; set; }
+        public int LeavePolicyID { get; set; }
+        public int LeaveTypeID { get; set; }
+        public int OpeningBalanceStatusID { get; set; }
+        public string LeaveOpeningBalanceCode { get; set; }
+        public DateTime LeaveOpeningBalanceDate { get; set; }
+        public int EmployeeID { get; set; }
+        public int BalanceDays { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class LeaveOpeningBalanceViewModel
+    {
+        public List<LeaveOpeningBalanceDTO> LeaveOpeningBalances { get; set; }
+        public LeaveOpeningBalanceDTO LeaveOpeningBalance { get; set; }
+    }
+}
