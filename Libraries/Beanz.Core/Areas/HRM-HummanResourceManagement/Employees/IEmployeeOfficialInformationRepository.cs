129e38f9b8c32721130aad29a9be868717769f6d
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:11:23 +0300

    Lovable: scaffold IEmployeeOfficialInformationRepository.cs for EmployeeOfficialInformations

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeOfficialInformationRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeOfficialInformationRepository
+    {
+        Task<List<EmployeeOfficialInformationDTO>> GetEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeOfficialInformationsAsync(BeanzCommonDTO lookup);
+        Task<EmployeeOfficialInformationViewModel> GetInfoEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<EmployeeOfficialInformationViewModel> PrintEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeOfficialInformationsAsync(BeanzCommonDTO common);
+    }
+}
