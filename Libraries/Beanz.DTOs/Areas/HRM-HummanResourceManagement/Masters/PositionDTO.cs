6a3b41fd7c186cf29e96eb4b4b65416a2137fbf2
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:35:51 +0300

    Lovable: scaffold PositionDTO.cs for Positions

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/PositionDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class PositionDTO
+    {
+        public int PositionID { get; set; }
+        public string PositionCode { get; set; }
+        public string PositionName { get; set; }
+        public string PositionAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class PositionViewModel
+    {
+        public List<PositionDTO> Positions { get; set; }
+        public PositionDTO Position { get; set; }
+    }
+}
