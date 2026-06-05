3e9dbadf4171fd7c1757f33087205abd69819bf9
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:40:17 +0300

    Lovable: scaffold WeekDayRepository.cs for WeekDays

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Masters/WeekDayRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Masters;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Masters
+{
+    public class WeekDayRepository : IWeekDayRepository
+    {
+        private readonly IMapper _mapper;
+
+        public WeekDayRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<WeekDayDTO>> GetWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<WeekDayDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpWeekDaysAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<WeekDayViewModel> GetInfoWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoMS_WeekDays";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _weekDayDTOList = new List<WeekDayDTO>();
+            bool _isWeekDaysConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var weekDayData = objTemp.Where(c => c.TableName == "MS_WeekDays");
+                        if (weekDayData.Count() > 0 && !_isWeekDaysConsumed)
+                        {
+                            _weekDayDTOList = _mapper.Map<List<WeekDayDTO>>(weekDayData);
+                            _isWeekDaysConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _weekDayDTO = _weekDayDTOList.Where(x => x.WeekDayID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new WeekDayViewModel
+                {
+                    WeekDays = _weekDayDTOList,
+                    WeekDay = _weekDayDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<WeekDayViewModel> PrintWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintMS_WeekDays";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _weekDayDTOList = new List<WeekDayDTO>();
+            bool _isWeekDaysConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var weekDayData = objTemp.Where(c => c.TableName == "MS_WeekDays");
+                        if (weekDayData.Count() > 0 && !_isWeekDaysConsumed)
+                        {
+                            _weekDayDTOList = _mapper.Map<List<WeekDayDTO>>(weekDayData);
+                            _isWeekDaysConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _weekDayDTO = _weekDayDTOList.Where(x => x.WeekDayID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new WeekDayViewModel
+                {
+                    WeekDays = _weekDayDTOList,
+                    WeekDay = _weekDayDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveWeekDaysAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApproveMS_WeekDays";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@WeekDayID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
