250a88991ffc4ac1f424963827272f064d14bba5
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:12:45 +0300

    Lovable: scaffold TicketPolicyPaymentDTO.cs for TicketPolicyPayments

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/TicketPolicyPaymentDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class TicketPolicyPaymentDTO
+    {
+        public int TicketPolicyPaymentID { get; set; }
+        public string TicketPolicyPaymentCode { get; set; }
+        public string TicketPolicyPaymentName { get; set; }
+        public string TicketPolicyPaymentAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class TicketPolicyPaymentViewModel
+    {
+        public List<TicketPolicyPaymentDTO> TicketPolicyPayments { get; set; }
+        public TicketPolicyPaymentDTO TicketPolicyPayment { get; set; }
+    }
+}
