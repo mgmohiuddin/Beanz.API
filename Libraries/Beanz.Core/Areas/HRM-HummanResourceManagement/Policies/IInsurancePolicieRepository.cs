fb2860135a7dca4a672ae1fe86f7fccf09c43b03
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:59:44 +0300

    Lovable: scaffold IInsurancePolicieRepository.cs for InsurancePolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IInsurancePolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IInsurancePolicieRepository
+    {
+        Task<List<InsurancePolicieDTO>> GetInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpInsurancePoliciesAsync(BeanzCommonDTO lookup);
+        Task<InsurancePolicieViewModel> GetInfoInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<InsurancePolicieViewModel> PrintInsurancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveInsurancePoliciesAsync(BeanzCommonDTO common);
+    }
+}
