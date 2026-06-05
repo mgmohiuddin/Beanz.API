1b1abf32536d0ccb58708142424ee65e853ad062
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:39:11 +0300

    Lovable: scaffold ITravelTypeRepository.cs for TravelTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/ITravelTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface ITravelTypeRepository
+    {
+        Task<List<TravelTypeDTO>> GetTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTravelTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTravelTypesAsync(BeanzCommonDTO lookup);
+        Task<TravelTypeViewModel> GetInfoTravelTypesAsync(BeanzCommonDTO common);
+        Task<TravelTypeViewModel> PrintTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTravelTypesAsync(BeanzCommonDTO common);
+    }
+}
