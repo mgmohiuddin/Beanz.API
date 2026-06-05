e7f29bb17d538ae5379e166e20767131c5788d92
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:52:17 +0300

    Lovable: scaffold AttendancePolicieBusiness.cs for AttendancePolicies

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/AttendancePolicieBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class AttendancePolicieBusiness
+    {
+        private readonly IAttendancePolicieRepository _repository;
+
+        public AttendancePolicieBusiness(IAttendancePolicieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<AttendancePolicieDTO>> GetAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetAttendancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> SetAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.SetAttendancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> PostAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.PostAttendancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> DelAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.DelAttendancePoliciesAsync(common);
+
+        public Task<AttendancePolicieViewModel> GetInfoAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoAttendancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveAttendancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.ApproveAttendancePoliciesAsync(common);
+    }
+}
