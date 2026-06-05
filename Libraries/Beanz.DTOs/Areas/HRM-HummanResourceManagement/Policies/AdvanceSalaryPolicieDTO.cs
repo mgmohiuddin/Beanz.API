ee1771edde55184334252115808d943056a1cd78
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:43:33 +0300

    Lovable: scaffold AdvanceSalaryPolicieDTO.cs for AdvanceSalaryPolicies

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/AdvanceSalaryPolicieDTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class AdvanceSalaryPolicieDTO
+    {
+        public int AdvanceSalaryPolicieID { get; set; }
+        public int AdvanceSalaryTypeID { get; set; }
+        public string AdvanceSalaryPolicyCode { get; set; }
+        public string AdvanceSalaryPolicyName { get; set; }
+        public string AdvanceSalaryPolicyAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class AdvanceSalaryPolicieViewModel
+    {
+        public List<AdvanceSalaryPolicieDTO> AdvanceSalaryPolicies { get; set; }
+        public AdvanceSalaryPolicieDTO AdvanceSalaryPolicie { get; set; }
+    }
+}
