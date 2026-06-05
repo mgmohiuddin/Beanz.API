fb2f8e246629a36ca312797156331072bbc0ebcd
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 00:04:25 +0300

    Lovable: scaffold IBusinessTripTypeRepository.cs for BusinessTripTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IBusinessTripTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IBusinessTripTypeRepository
+    {
+        Task<List<BusinessTripTypeDTO>> GetBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpBusinessTripTypesAsync(BeanzCommonDTO lookup);
+        Task<BusinessTripTypeViewModel> GetInfoBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<BusinessTripTypeViewModel> PrintBusinessTripTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveBusinessTripTypesAsync(BeanzCommonDTO common);
+    }
+}
