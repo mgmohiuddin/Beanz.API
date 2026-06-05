597c36b47b7cb08326abd732a98cc6b93fe53a2e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:52:40 +0300

    Lovable: scaffold DocumentSubmissionTypeDTO.cs for DocumentSubmissionTypes

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/DocumentSubmissionTypeDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class DocumentSubmissionTypeDTO
+    {
+        public int DocumentSubmissionTypeID { get; set; }
+        public string DocumentSubmissionTypeCode { get; set; }
+        public string DocumentSubmissionTypeName { get; set; }
+        public string DocumentSubmissionTypeAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class DocumentSubmissionTypeViewModel
+    {
+        public List<DocumentSubmissionTypeDTO> DocumentSubmissionTypes { get; set; }
+        public DocumentSubmissionTypeDTO DocumentSubmissionType { get; set; }
+    }
+}
