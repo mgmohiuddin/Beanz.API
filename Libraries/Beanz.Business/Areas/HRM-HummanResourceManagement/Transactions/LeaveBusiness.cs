e5af0086b85b84fb8c5646984d07104280af59e6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:19:41 +0300

    Lovable: scaffold LeaveBusiness.cs for Leaves

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Transactions/LeaveBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Transactions
+{
+    public class LeaveBusiness
+    {
+        private readonly ILeaveRepository _repository;
+
+        public LeaveBusiness(ILeaveRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<LeaveDTO>> GetLeavesAsync(BeanzCommonDTO common)
+            => _repository.GetLeavesAsync(common);
+
+        public Task<BeanzResponseDTO> SetLeavesAsync(BeanzCommonDTO common)
+            => _repository.SetLeavesAsync(common);
+
+        public Task<BeanzResponseDTO> PostLeavesAsync(BeanzCommonDTO common)
+            => _repository.PostLeavesAsync(common);
+
+        public Task<BeanzResponseDTO> DelLeavesAsync(BeanzCommonDTO common)
+            => _repository.DelLeavesAsync(common);
+
+        public Task<LeaveViewModel> GetInfoLeavesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoLeavesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveLeavesAsync(BeanzCommonDTO common)
+            => _repository.ApproveLeavesAsync(common);
+    }
+}
