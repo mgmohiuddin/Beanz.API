41816b4e3cbd372a92de3575540937bef16afb38
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:14:42 +0300

    Lovable: scaffold EmployeeContractAllowanceBusiness.cs for EmployeeContractAllowances

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeContractAllowanceBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeContractAllowanceBusiness
+    {
+        private readonly IEmployeeContractAllowanceRepository _repository;
+
+        public EmployeeContractAllowanceBusiness(IEmployeeContractAllowanceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeContractAllowanceDTO>> GetEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeContractAllowancesAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeContractAllowancesAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeContractAllowancesAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeContractAllowancesAsync(common);
+
+        public Task<EmployeeContractAllowanceViewModel> GetInfoEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeContractAllowancesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeContractAllowancesAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeContractAllowancesAsync(common);
+    }
+}
