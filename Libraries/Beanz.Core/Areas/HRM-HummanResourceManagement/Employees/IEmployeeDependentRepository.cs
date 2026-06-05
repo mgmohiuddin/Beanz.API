8cb6c3c95d140d43ea6a8cde8eb8a1d0c5c8442b
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:13:26 +0300

    Lovable: scaffold IEmployeeDependentRepository.cs for EmployeeDependents

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeDependentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeDependentRepository
+    {
+        Task<List<EmployeeDependentDTO>> GetEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeDependentsAsync(BeanzCommonDTO lookup);
+        Task<EmployeeDependentViewModel> GetInfoEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<EmployeeDependentViewModel> PrintEmployeeDependentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeDependentsAsync(BeanzCommonDTO common);
+    }
+}
