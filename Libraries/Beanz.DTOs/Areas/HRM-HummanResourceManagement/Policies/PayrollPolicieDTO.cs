5eedfb6738a6f540d9a6af06d851b17efbfd6d04
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:09:59 +0300

    Lovable: scaffold PayrollPolicieDTO.cs for PayrollPolicies

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/PayrollPolicieDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class PayrollPolicieDTO
+    {
+        public int PayrollPolicieID { get; set; }
+        public string PayrollPolicyCode { get; set; }
+        public string PayrollPolicyName { get; set; }
+        public string PayrollPolicyAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class PayrollPolicieViewModel
+    {
+        public List<PayrollPolicieDTO> PayrollPolicies { get; set; }
+        public PayrollPolicieDTO PayrollPolicie { get; set; }
+    }
+}
