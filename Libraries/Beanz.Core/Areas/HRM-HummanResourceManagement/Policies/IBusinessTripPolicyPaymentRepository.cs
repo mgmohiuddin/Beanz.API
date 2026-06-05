16093885fa78cb625c80bad25cfce34ab3f7a092
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:54:59 +0300

    Lovable: scaffold IBusinessTripPolicyPaymentRepository.cs for BusinessTripPolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IBusinessTripPolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IBusinessTripPolicyPaymentRepository
+    {
+        Task<List<BusinessTripPolicyPaymentDTO>> GetBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpBusinessTripPolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<BusinessTripPolicyPaymentViewModel> GetInfoBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BusinessTripPolicyPaymentViewModel> PrintBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveBusinessTripPolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
