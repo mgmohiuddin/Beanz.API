0cdf9067fb2ddf5c43e6a8dae62ec0f877640120
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:14:21 +0300

    Lovable: scaffold EmployeeContractOfficialInformationBusiness.cs for EmployeeContractOfficialInformations

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeContractOfficialInformationBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeContractOfficialInformationBusiness
+    {
+        private readonly IEmployeeContractOfficialInformationRepository _repository;
+
+        public EmployeeContractOfficialInformationBusiness(IEmployeeContractOfficialInformationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeContractOfficialInformationDTO>> GetEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeContractOfficialInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeContractOfficialInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeContractOfficialInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeContractOfficialInformationsAsync(common);
+
+        public Task<EmployeeContractOfficialInformationViewModel> GetInfoEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeContractOfficialInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeContractOfficialInformationsAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeContractOfficialInformationsAsync(common);
+    }
+}
