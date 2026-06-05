cfde445118d0b1be12e38da13d4e0e12101add66
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:31:26 +0300

    Lovable: scaffold IDocumentTypeRepository.cs for DocumentTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IDocumentTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IDocumentTypeRepository
+    {
+        Task<List<DocumentTypeDTO>> GetDocumentTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetDocumentTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostDocumentTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelDocumentTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpDocumentTypesAsync(BeanzCommonDTO lookup);
+        Task<DocumentTypeViewModel> GetInfoDocumentTypesAsync(BeanzCommonDTO common);
+        Task<DocumentTypeViewModel> PrintDocumentTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveDocumentTypesAsync(BeanzCommonDTO common);
+    }
+}
