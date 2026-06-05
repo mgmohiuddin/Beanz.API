00b7b74fe0b3ae5837656531e3a28812c3a01dbc
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:13:33 +0300

    Lovable: scaffold IVacationPolicieRepository.cs for VacationPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IVacationPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IVacationPolicieRepository
+    {
+        Task<List<VacationPolicieDTO>> GetVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpVacationPoliciesAsync(BeanzCommonDTO lookup);
+        Task<VacationPolicieViewModel> GetInfoVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<VacationPolicieViewModel> PrintVacationPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveVacationPoliciesAsync(BeanzCommonDTO common);
+    }
+}
