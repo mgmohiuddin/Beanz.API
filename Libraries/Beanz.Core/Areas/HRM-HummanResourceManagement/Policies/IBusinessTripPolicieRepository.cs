de6830b6195c6c2df93e143384322772df3fb763
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:54:18 +0300

    Lovable: scaffold IBusinessTripPolicieRepository.cs for BusinessTripPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IBusinessTripPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IBusinessTripPolicieRepository
+    {
+        Task<List<BusinessTripPolicieDTO>> GetBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpBusinessTripPoliciesAsync(BeanzCommonDTO lookup);
+        Task<BusinessTripPolicieViewModel> GetInfoBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<BusinessTripPolicieViewModel> PrintBusinessTripPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveBusinessTripPoliciesAsync(BeanzCommonDTO common);
+    }
+}
