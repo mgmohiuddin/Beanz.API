20209ffd4eaea0a941a0ee69cf55de9c9f0d13a7
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:01:12 +0300

    Lovable: scaffold LeavePolicyPaymentBusiness.cs for LeavePolicyPayments

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/LeavePolicyPaymentBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class LeavePolicyPaymentBusiness
+    {
+        private readonly ILeavePolicyPaymentRepository _repository;
+
+        public LeavePolicyPaymentBusiness(ILeavePolicyPaymentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<LeavePolicyPaymentDTO>> GetLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetLeavePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> SetLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.SetLeavePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> PostLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.PostLeavePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> DelLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.DelLeavePolicyPaymentsAsync(common);
+
+        public Task<LeavePolicyPaymentViewModel> GetInfoLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoLeavePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveLeavePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.ApproveLeavePolicyPaymentsAsync(common);
+    }
+}
