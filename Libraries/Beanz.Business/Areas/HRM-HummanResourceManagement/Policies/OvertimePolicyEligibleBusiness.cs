2cdf8a9af9446935bc2b4641c877640c44f4e018
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:08:20 +0300

    Lovable: scaffold OvertimePolicyEligibleBusiness.cs for OvertimePolicyEligibles

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/OvertimePolicyEligibleBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class OvertimePolicyEligibleBusiness
+    {
+        private readonly IOvertimePolicyEligibleRepository _repository;
+
+        public OvertimePolicyEligibleBusiness(IOvertimePolicyEligibleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<OvertimePolicyEligibleDTO>> GetOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetOvertimePolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> SetOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.SetOvertimePolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> PostOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.PostOvertimePolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> DelOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.DelOvertimePolicyEligiblesAsync(common);
+
+        public Task<OvertimePolicyEligibleViewModel> GetInfoOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoOvertimePolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveOvertimePolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.ApproveOvertimePolicyEligiblesAsync(common);
+    }
+}
