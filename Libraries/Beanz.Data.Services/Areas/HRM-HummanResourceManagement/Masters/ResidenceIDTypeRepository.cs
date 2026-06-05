db3ef1b3d8a6a544a06dd3253a375c6dd3a32a63
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:36:19 +0300

    Lovable: scaffold ResidenceIDTypeRepository.cs for ResidenceIDTypes

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Masters/ResidenceIDTypeRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Masters;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Masters
+{
+    public class ResidenceIDTypeRepository : IResidenceIDTypeRepository
+    {
+        private readonly IMapper _mapper;
+
+        public ResidenceIDTypeRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResidenceIDTypeDTO>> GetResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<ResidenceIDTypeDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpResidenceIDTypesAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<ResidenceIDTypeViewModel> GetInfoResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoMS_ResidenceIDTypes";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _residenceIDTypeDTOList = new List<ResidenceIDTypeDTO>();
+            bool _isResidenceIDTypesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var residenceIDTypeData = objTemp.Where(c => c.TableName == "MS_ResidenceIDTypes");
+                        if (residenceIDTypeData.Count() > 0 && !_isResidenceIDTypesConsumed)
+                        {
+                            _residenceIDTypeDTOList = _mapper.Map<List<ResidenceIDTypeDTO>>(residenceIDTypeData);
+                            _isResidenceIDTypesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _residenceIDTypeDTO = _residenceIDTypeDTOList.Where(x => x.ResidenceIDTypeID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new ResidenceIDTypeViewModel
+                {
+                    ResidenceIDTypes = _residenceIDTypeDTOList,
+                    ResidenceIDType = _residenceIDTypeDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<ResidenceIDTypeViewModel> PrintResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintMS_ResidenceIDTypes";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _residenceIDTypeDTOList = new List<ResidenceIDTypeDTO>();
+            bool _isResidenceIDTypesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var residenceIDTypeData = objTemp.Where(c => c.TableName == "MS_ResidenceIDTypes");
+                        if (residenceIDTypeData.Count() > 0 && !_isResidenceIDTypesConsumed)
+                        {
+                            _residenceIDTypeDTOList = _mapper.Map<List<ResidenceIDTypeDTO>>(residenceIDTypeData);
+                            _isResidenceIDTypesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _residenceIDTypeDTO = _residenceIDTypeDTOList.Where(x => x.ResidenceIDTypeID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new ResidenceIDTypeViewModel
+                {
+                    ResidenceIDTypes = _residenceIDTypeDTOList,
+                    ResidenceIDType = _residenceIDTypeDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveResidenceIDTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApproveMS_ResidenceIDTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@ResidenceIDTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
