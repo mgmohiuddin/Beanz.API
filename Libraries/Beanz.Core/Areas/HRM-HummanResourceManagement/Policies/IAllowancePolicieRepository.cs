a6fd09360e7921a62d5ee7962ac7628cefe5d16a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:48:32 +0300

    Lovable: scaffold IAllowancePolicieRepository.cs for AllowancePolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAllowancePolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAllowancePolicieRepository
+    {
+        Task<List<AllowancePolicieDTO>> GetAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAllowancePoliciesAsync(BeanzCommonDTO lookup);
+        Task<AllowancePolicieViewModel> GetInfoAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<AllowancePolicieViewModel> PrintAllowancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAllowancePoliciesAsync(BeanzCommonDTO common);
+    }
+}
