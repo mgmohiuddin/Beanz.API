998044c4a9d16daae88804aadf5aab3b2ec57ff4
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:13:58 +0300

    Lovable: scaffold IEmployeeContractRepository.cs for EmployeeContracts

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeContractRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeContractRepository
+    {
+        Task<List<EmployeeContractDTO>> GetEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeContractsAsync(BeanzCommonDTO lookup);
+        Task<EmployeeContractViewModel> GetInfoEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<EmployeeContractViewModel> PrintEmployeeContractsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeContractsAsync(BeanzCommonDTO common);
+    }
+}
