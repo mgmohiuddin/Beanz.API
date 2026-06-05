6cb795ca6eeb5428fdd03b13052a26c16ac4024e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:31:53 +0300

    Lovable: scaffold BusinessTripPayTypeBusiness.cs for BusinessTripPayTypes

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Masters/BusinessTripPayTypeBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Areas.Common;
+using Beanz.DTOs.Areas.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Masters
+{
+    public class BusinessTripPayTypeBusiness
+    {
+        private readonly IBusinessTripPayTypeRepository _repository;
+
+        public BusinessTripPayTypeBusiness(IBusinessTripPayTypeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<BusinessTripPayTypeDTO>> GetBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.GetBusinessTripPayTypesAsync(common);
+
+        public Task<BeanzResponseDTO> SetBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.SetBusinessTripPayTypesAsync(common);
+
+        public Task<BeanzResponseDTO> PostBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.PostBusinessTripPayTypesAsync(common);
+
+        public Task<BeanzResponseDTO> DelBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.DelBusinessTripPayTypesAsync(common);
+
+        public Task<BusinessTripPayTypeViewModel> GetInfoBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoBusinessTripPayTypesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveBusinessTripPayTypesAsync(BeanzCommonDTO common)
+            => _repository.ApproveBusinessTripPayTypesAsync(common);
+    }
+}
