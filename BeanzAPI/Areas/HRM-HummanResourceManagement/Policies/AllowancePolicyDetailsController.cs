0f301be8a5a365b10c190b4b8406ca0b49fc808a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:44:40 +0300

    Lovable: scaffold AllowancePolicyDetailsController.cs for AllowancePolicyDetails

---
+++ BeanzAPI/Areas/HRM-HummanResourceManagement/Policies/AllowancePolicyDetailsController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.HummanResourceManagement.Policies
+{
+    [Route("api/[area]/Policies/[controller]/[action]")]
+    [ApiController]
+    [Area("HummanResourceManagement")]
+    public class AllowancePolicyDetailsController : ControllerBase
+    {
+        private readonly IAllowancePolicyDetailRepository _allowancePolicyDetailsRepository;
+
+        public AllowancePolicyDetailsController(IAllowancePolicyDetailRepository allowancePolicyDetailsRepository)
+        {
+            _allowancePolicyDetailsRepository = allowancePolicyDetailsRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<AllowancePolicyDetailDTO>> GetAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            var data = await _allowancePolicyDetailsRepository.GetAllowancePolicyDetailsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _allowancePolicyDetailsRepository.SetAllowancePolicyDetailsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _allowancePolicyDetailsRepository.PostAllowancePolicyDetailsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _allowancePolicyDetailsRepository.DelAllowancePolicyDetailsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpAllowancePolicyDetails(BeanzCommonDTO lookup)
+        {
+            var data = await _allowancePolicyDetailsRepository.LookUpAllowancePolicyDetailsAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<AllowancePolicyDetailViewModel> GetInfoAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            var data = await _allowancePolicyDetailsRepository.GetInfoAllowancePolicyDetailsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<AllowancePolicyDetailViewModel> PrintAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            var data = await _allowancePolicyDetailsRepository.PrintAllowancePolicyDetailsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveAllowancePolicyDetails(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _allowancePolicyDetailsRepository.ApproveAllowancePolicyDetailsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
