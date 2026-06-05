41f879f65015dbbdce768b5abf874a8684dd97dd
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:15:40 +0300

    Lovable: scaffold EmployeeCalendarDayShiftBusiness.cs for EmployeeCalendarDayShifts

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeCalendarDayShiftBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeCalendarDayShiftBusiness
+    {
+        private readonly IEmployeeCalendarDayShiftRepository _repository;
+
+        public EmployeeCalendarDayShiftBusiness(IEmployeeCalendarDayShiftRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeCalendarDayShiftDTO>> GetEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeCalendarDayShiftsAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeCalendarDayShiftsAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeCalendarDayShiftsAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeCalendarDayShiftsAsync(common);
+
+        public Task<EmployeeCalendarDayShiftViewModel> GetInfoEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeCalendarDayShiftsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeCalendarDayShiftsAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeCalendarDayShiftsAsync(common);
+    }
+}
