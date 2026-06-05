9da75ce1ac013a857de04f3cbb13d43ac64018bc
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:32:09 +0300

    Lovable: scaffold IEndOfServiceTypeRepository.cs for EndOfServiceTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IEndOfServiceTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IEndOfServiceTypeRepository
+    {
+        Task<List<EndOfServiceTypeDTO>> GetEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEndOfServiceTypesAsync(BeanzCommonDTO lookup);
+        Task<EndOfServiceTypeViewModel> GetInfoEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<EndOfServiceTypeViewModel> PrintEndOfServiceTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEndOfServiceTypesAsync(BeanzCommonDTO common);
+    }
+}
