3dbe4132fb8c1836e3f9e973e3026f260ff29baf
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:56:11 +0300

    Lovable: scaffold EndOfServicePolicySalaryAllowanceDTO.cs for EndOfServicePolicySalaryAllowances

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/EndOfServicePolicySalaryAllowanceDTO.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class EndOfServicePolicySalaryAllowanceDTO
+    {
+        public int EndOfServicePolicySalaryAllowanceID { get; set; }
+        public int EndOfServicePolicyID { get; set; }
+        public int AllowanceTypeID { get; set; }
+        public int AllowanceID { get; set; }
+        public string EndOfServicePolicySalaryAllowanceCode { get; set; }
+        public string EndOfServicePolicySalaryAllowanceName { get; set; }
+        public string EndOfServicePolicySalaryAllowanceAlias { get; set; }
+        public decimal Percentage { get; set; }
+        public bool IsSalaryCalculateLastWorkingDate { get; set; }
+        public bool IsPaidRecover { get; set; }
+        public bool IsNoticePeriod { get; set; }
+        public bool IsComingSalary { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class EndOfServicePolicySalaryAllowanceViewModel
+    {
+        public List<EndOfServicePolicySalaryAllowanceDTO> EndOfServicePolicySalaryAllowances { get; set; }
+        public EndOfServicePolicySalaryAllowanceDTO EndOfServicePolicySalaryAllowance { get; set; }
+    }
+}
