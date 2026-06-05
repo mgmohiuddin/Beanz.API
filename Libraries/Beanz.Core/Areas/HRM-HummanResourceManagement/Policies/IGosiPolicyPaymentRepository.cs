232d8960be840c63a79b589883ccbbf87c3e84f7
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:58:03 +0300

    Lovable: scaffold IGosiPolicyPaymentRepository.cs for GosiPolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IGosiPolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IGosiPolicyPaymentRepository
+    {
+        Task<List<GosiPolicyPaymentDTO>> GetGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpGosiPolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<GosiPolicyPaymentViewModel> GetInfoGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<GosiPolicyPaymentViewModel> PrintGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveGosiPolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
