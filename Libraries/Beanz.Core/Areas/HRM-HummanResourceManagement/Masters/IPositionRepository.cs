5533024fda1a0a356da35f0dd4af1c5838f09627
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:35:47 +0300

    Lovable: scaffold IPositionRepository.cs for Positions

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IPositionRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IPositionRepository
+    {
+        Task<List<PositionDTO>> GetPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPositionsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPositionsAsync(BeanzCommonDTO lookup);
+        Task<PositionViewModel> GetInfoPositionsAsync(BeanzCommonDTO common);
+        Task<PositionViewModel> PrintPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePositionsAsync(BeanzCommonDTO common);
+    }
+}
