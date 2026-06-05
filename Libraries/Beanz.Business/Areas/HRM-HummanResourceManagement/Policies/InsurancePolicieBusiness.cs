3d853525b8fd818d11746b8e5bafc25b0fc38d5b
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:59:50 +0300

    Lovable: scaffold InsurancePolicieBusiness.cs for InsurancePolicies

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/InsurancePolicieBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class InsurancePolicieBusiness
+    {
+        private readonly IInsurancePolicieRepository _repository;
+
+        public InsurancePolicieBusiness(IInsurancePolicieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<InsurancePolicieDTO>> GetInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetInsurancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> SetInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.SetInsurancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> PostInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.PostInsurancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> DelInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.DelInsurancePoliciesAsync(common);
+
+        public Task<InsurancePolicieViewModel> GetInfoInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoInsurancePoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveInsurancePoliciesAsync(BeanzCommonDTO common)
+            => _repository.ApproveInsurancePoliciesAsync(common);
+    }
+}
