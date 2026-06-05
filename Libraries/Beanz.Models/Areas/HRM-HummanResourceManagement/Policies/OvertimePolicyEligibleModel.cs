1aa8d64c437d7bd374f23a8653bb4ff55d1eee07
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:08:19 +0300

    Lovable: scaffold OvertimePolicyEligibleModel.cs for OvertimePolicyEligibles

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Policies/OvertimePolicyEligibleModel.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Policies.Objects
+{
+    // Entity model generated from table structure
+    public class OvertimePolicyEligible
+    {
+        public int OvertimePolicyEligibleID { get; set; }
+        public int AreaID { get; set; }
+        public int BranchID { get; set; }
+        public int CityID { get; set; }
+        public int CountryID { get; set; }
+        public int DepartmentID { get; set; }
+        public int GenderID { get; set; }
+        public int GradeID { get; set; }
+        public int MaritialStatusID { get; set; }
+        public int NationalityID { get; set; }
+        public int OrganizationID { get; set; }
+        public int PositionID { get; set; }
+        public int RegionID { get; set; }
+        public int ReligionID { get; set; }
+        public int SectionID { get; set; }
+        public int SubDepartmentID { get; set; }
+        public int SubGradeID { get; set; }
+        public int SubOrganizationID { get; set; }
+        public int SubSectionID { get; set; }
+        public int OverTimePolicyID { get; set; }
+        public string OvertimePolicyEligibleCode { get; set; }
+        public string OvertimePolicyEligibleName { get; set; }
+        public string OvertimePolicyEligibleAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
