6806485c6befc0a6da3f346d6025b6562091880a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:52:08 +0300

    Lovable: scaffold AllowancesGroupDTO.cs for AllowancesGroups

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/AllowancesGroupDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class AllowancesGroupDTO
+    {
+        public int AllowancesGroupID { get; set; }
+        public string AllowancesGroupCode { get; set; }
+        public string AllowancesGroupName { get; set; }
+        public string AllowancesGroupAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class AllowancesGroupViewModel
+    {
+        public List<AllowancesGroupDTO> AllowancesGroups { get; set; }
+        public AllowancesGroupDTO AllowancesGroup { get; set; }
+    }
+}
