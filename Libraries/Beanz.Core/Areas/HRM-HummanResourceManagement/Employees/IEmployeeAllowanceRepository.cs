6350f224e769d1b8c091f9f3337416a5be9843ed
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:16:26 +0300

    Lovable: scaffold IEmployeeAllowanceRepository.cs for EmployeeAllowances

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeAllowanceRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeAllowanceRepository
+    {
+        Task<List<EmployeeAllowanceDTO>> GetEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeAllowancesAsync(BeanzCommonDTO lookup);
+        Task<EmployeeAllowanceViewModel> GetInfoEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<EmployeeAllowanceViewModel> PrintEmployeeAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeAllowancesAsync(BeanzCommonDTO common);
+    }
+}
