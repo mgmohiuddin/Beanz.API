6924a99237429c4eacdf1350231dd38898d18d24
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:49:28 +0300

    Lovable: scaffold AllowancePolicyPaymentBusiness.cs for AllowancePolicyPayments

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/AllowancePolicyPaymentBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class AllowancePolicyPaymentBusiness
+    {
+        private readonly IAllowancePolicyPaymentRepository _repository;
+
+        public AllowancePolicyPaymentBusiness(IAllowancePolicyPaymentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<AllowancePolicyPaymentDTO>> GetAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetAllowancePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> SetAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.SetAllowancePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> PostAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.PostAllowancePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> DelAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.DelAllowancePolicyPaymentsAsync(common);
+
+        public Task<AllowancePolicyPaymentViewModel> GetInfoAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoAllowancePolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveAllowancePolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.ApproveAllowancePolicyPaymentsAsync(common);
+    }
+}
