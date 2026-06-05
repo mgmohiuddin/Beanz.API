cd8c59380e7c06a3f4bfe0f7f7eec5c0106d85bc
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:11:50 +0300

    Lovable: scaffold EmployeeFinancialInformationDTO.cs for EmployeeFinancialInformations

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Employees/EmployeeFinancialInformationDTO.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeFinancialInformationDTO
+    {
+        public int EmployeeFinancialInformationID { get; set; }
+        public int BankID { get; set; }
+        public int BankAccountTypeID { get; set; }
+        public string EmployeeFinancialInformationCode { get; set; }
+        public string EmployeeFinancialInformationName { get; set; }
+        public string EmployeeFinancialInformationAlias { get; set; }
+        public int EmployeeID { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string IBANNumber { get; set; }
+        public bool IsSalaryBank { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class EmployeeFinancialInformationViewModel
+    {
+        public List<EmployeeFinancialInformationDTO> EmployeeFinancialInformations { get; set; }
+        public EmployeeFinancialInformationDTO EmployeeFinancialInformation { get; set; }
+    }
+}
