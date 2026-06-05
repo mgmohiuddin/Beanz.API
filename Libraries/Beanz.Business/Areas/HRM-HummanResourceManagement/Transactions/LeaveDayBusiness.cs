00622aff8adefa7dfa0464def9e1090097109287
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:20:17 +0300

    Lovable: scaffold LeaveDayBusiness.cs for LeaveDays

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Transactions/LeaveDayBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Transactions
+{
+    public class LeaveDayBusiness
+    {
+        private readonly ILeaveDayRepository _repository;
+
+        public LeaveDayBusiness(ILeaveDayRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<LeaveDayDTO>> GetLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.GetLeaveDaysAsync(common);
+
+        public Task<BeanzResponseDTO> SetLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.SetLeaveDaysAsync(common);
+
+        public Task<BeanzResponseDTO> PostLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.PostLeaveDaysAsync(common);
+
+        public Task<BeanzResponseDTO> DelLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.DelLeaveDaysAsync(common);
+
+        public Task<LeaveDayViewModel> GetInfoLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.GetInfoLeaveDaysAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveLeaveDaysAsync(BeanzCommonDTO common)
+            => _repository.ApproveLeaveDaysAsync(common);
+    }
+}
