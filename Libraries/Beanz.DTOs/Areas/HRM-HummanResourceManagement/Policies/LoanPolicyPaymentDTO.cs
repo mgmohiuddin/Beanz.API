29d082bf352cd4a6af65f690529818d3f77d8aea
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:06:35 +0300

    Lovable: scaffold LoanPolicyPaymentDTO.cs for LoanPolicyPayments

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/LoanPolicyPaymentDTO.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class LoanPolicyPaymentDTO
+    {
+        public int LoanPolicyPaymentID { get; set; }
+        public int AllowanceID { get; set; }
+        public int AllowanceTypeID { get; set; }
+        public int LoanPolicyID { get; set; }
+        public string LoanPolicyPaymentCode { get; set; }
+        public string LoanPolicyPaymentName { get; set; }
+        public string LoanPolicyPaymentAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class LoanPolicyPaymentViewModel
+    {
+        public List<LoanPolicyPaymentDTO> LoanPolicyPayments { get; set; }
+        public LoanPolicyPaymentDTO LoanPolicyPayment { get; set; }
+    }
+}
