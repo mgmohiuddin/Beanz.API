1fb226215a78e7fd4d3ae367e313bd463fc1a227
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:19:58 +0300

    Lovable: scaffold LeaveOpeningBalanceModel.cs for LeaveOpeningBalances

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Transactions/LeaveOpeningBalanceModel.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Transactions.Objects
+{
+    // Entity model generated from table structure
+    public class LeaveOpeningBalance
+    {
+        public int LeaveOpeningBalanceID { get; set; }
+        public int LeavePolicyID { get; set; }
+        public int LeaveTypeID { get; set; }
+        public int OpeningBalanceStatusID { get; set; }
+        public string LeaveOpeningBalanceCode { get; set; }
+        public DateTime LeaveOpeningBalanceDate { get; set; }
+        public int EmployeeID { get; set; }
+        public int BalanceDays { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
