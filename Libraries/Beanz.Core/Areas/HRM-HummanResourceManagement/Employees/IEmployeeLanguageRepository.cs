e161bae20f04dd4cd6c5f50262e3273fa5ab0132
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:11:39 +0300

    Lovable: scaffold IEmployeeLanguageRepository.cs for EmployeeLanguages

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeLanguageRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeLanguageRepository
+    {
+        Task<List<EmployeeLanguageDTO>> GetEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeLanguagesAsync(BeanzCommonDTO lookup);
+        Task<EmployeeLanguageViewModel> GetInfoEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<EmployeeLanguageViewModel> PrintEmployeeLanguagesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeLanguagesAsync(BeanzCommonDTO common);
+    }
+}
