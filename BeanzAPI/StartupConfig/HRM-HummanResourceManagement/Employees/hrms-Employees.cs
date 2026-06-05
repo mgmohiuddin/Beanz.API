e39f50114a507b77a8f966a05e00539e505181ae
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:15:03 +0300

    Lovable: scaffold hrms-Employees.cs (module startup registrations)

--- BeanzAPI/StartupConfig/HRM-HummanResourceManagement/Employees/hrms-Employees.cs
+++ BeanzAPI/StartupConfig/HRM-HummanResourceManagement/Employees/hrms-Employees.cs
@@ -7,6 +7,17 @@ namespace Beanz.API.StartupConfig.HummanResourceManagement.Employees
     {
         public static void Register(IServiceCollection services)
         {
+            services.AddScoped<IEmployeeContactInformationRepository, EmployeeContactInformationRepository>();
+            services.AddScoped<IEmployeeContractAllowanceRepository, EmployeeContractAllowanceRepository>();
+            services.AddScoped<IEmployeeContractOfficialInformationRepository, EmployeeContractOfficialInformationRepository>();
+            services.AddScoped<IEmployeeContractRepository, EmployeeContractRepository>();
+            services.AddScoped<IEmployeeDependentDocumentRepository, EmployeeDependentDocumentRepository>();
+            services.AddScoped<IEmployeeDependentRepository, EmployeeDependentRepository>();
+            services.AddScoped<IEmployeeDocumentRepository, EmployeeDocumentRepository>();
+            services.AddScoped<IEmployeeFinancialInformationRepository, EmployeeFinancialInformationRepository>();
+            services.AddScoped<IEmployeeLanguageRepository, EmployeeLanguageRepository>();
+            services.AddScoped<IEmployeeOfficialInformationRepository, EmployeeOfficialInformationRepository>();
+            services.AddScoped<IEmployeeQualificationRepository, EmployeeQualificationRepository>();
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
         }
     }
