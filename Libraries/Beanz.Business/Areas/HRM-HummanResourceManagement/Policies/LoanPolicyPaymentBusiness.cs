ea1ce660c4f7a1087df7bc6e81ce4a558256e194
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:06:39 +0300

    Lovable: scaffold LoanPolicyPaymentBusiness.cs for LoanPolicyPayments

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/LoanPolicyPaymentBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class LoanPolicyPaymentBusiness
+    {
+        private readonly ILoanPolicyPaymentRepository _repository;
+
+        public LoanPolicyPaymentBusiness(ILoanPolicyPaymentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<LoanPolicyPaymentDTO>> GetLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetLoanPolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> SetLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.SetLoanPolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> PostLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.PostLoanPolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> DelLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.DelLoanPolicyPaymentsAsync(common);
+
+        public Task<LoanPolicyPaymentViewModel> GetInfoLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoLoanPolicyPaymentsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveLoanPolicyPaymentsAsync(BeanzCommonDTO common)
+            => _repository.ApproveLoanPolicyPaymentsAsync(common);
+    }
+}
