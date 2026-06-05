cc6da85a5ed50bd33c675eae4fbf89730d4b9b0b
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:42:00 +0300

    Lovable: scaffold AssetTypeDTO.cs for AssetTypes

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/AssetTypeDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class AssetTypeDTO
+    {
+        public int AssetTypeID { get; set; }
+        public string AssetTypeCode { get; set; }
+        public string AssetTypeName { get; set; }
+        public string AssetTypeAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class AssetTypeViewModel
+    {
+        public List<AssetTypeDTO> AssetTypes { get; set; }
+        public AssetTypeDTO AssetType { get; set; }
+    }
+}
