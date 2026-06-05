eee166504cada7c3d3be1e40ca08c44236dbbd7f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:52:46 +0300

    Lovable: scaffold IAttendancePolicyEligibleRepository.cs for AttendancePolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAttendancePolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAttendancePolicyEligibleRepository
+    {
+        Task<List<AttendancePolicyEligibleDTO>> GetAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAttendancePolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<AttendancePolicyEligibleViewModel> GetInfoAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<AttendancePolicyEligibleViewModel> PrintAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAttendancePolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
