a2a2583d9e73d50da0d10e7e160a40ddd3ca5af5
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:32:28 +0300

    Lovable: scaffold EmployeeBusiness.cs for Employees

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeBusiness
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public EmployeeBusiness(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeDTO>> GetEmployeesAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeesAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeesAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeesAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeesAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeesAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeesAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeesAsync(common);
+
+        public Task<EmployeeViewModel> GetInfoEmployeesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeesAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeesAsync(common);
+    }
+}
