fd88543929a0e3b085330003290d56ff2a02c0a0
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 23:28:13 +0300

    Lovable: scaffold SystemModuleModel.cs for SystemModules

---
+++ Libraries/Beanz.Models/Areas/System/Setup/SystemModuleModel.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Beanz.Models.Areas.System.Setup.Objects
+{
+    // Entity model generated from table structure
+    public class SystemModule
+    {
+        public int SystemModuleID { get; set; }
+        public string SystemModuleCode { get; set; }
+        public string SystemModuleName { get; set; }
+        public string SystemModuleAlias { get; set; }
+        public string IconTag { get; set; }
+        public string ParentSystemModuleCode { get; set; }
+        public int ParentSystemModuleID { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
