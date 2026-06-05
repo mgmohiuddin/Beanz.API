fd5ac7733f58d0e8f3c3a9c909ccf741b7369eb4
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:09:18 +0300

    Lovable: scaffold PaidTimeOffPolicyEligibleModel.cs for PaidTimeOffPolicyEligibles

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Policies/PaidTimeOffPolicyEligibleModel.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Policies.Objects
+{
+    // Entity model generated from table structure
+    public class PaidTimeOffPolicyEligible
+    {
+        public int PaidTimeOffPolicyEligibleID { get; set; }
+        public string PaidTimeOffPolicyEligibleCode { get; set; }
+        public string PaidTimeOffPolicyEligibleName { get; set; }
+        public string PaidTimeOffPolicyEligibleAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
