87642895d0f78e1b74f579ed6902cc3fca94d5e6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:55:55 +0300

    Lovable: scaffold EndOfServicePolicyCalculationModel.cs for EndOfServicePolicyCalculations

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Policies/EndOfServicePolicyCalculationModel.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Policies.Objects
+{
+    // Entity model generated from table structure
+    public class EndOfServicePolicyCalculation
+    {
+        public int EndOfServicePolicyCalculationID { get; set; }
+        public int EndOfServicePolicyID { get; set; }
+        public string EndOfServicePolicyCalculationCode { get; set; }
+        public string EndOfServicePolicyCalculationName { get; set; }
+        public string EndOfServicePolicyCalculationAlias { get; set; }
+        public int FromYear { get; set; }
+        public int ToYear { get; set; }
+        public decimal Percentage { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
