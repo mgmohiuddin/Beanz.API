a0bafe4bd4aab53991d2122fa3f349bf445e51ea
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:57:26 +0300

    Lovable: scaffold GosiPolicyDetailDTO.cs for GosiPolicyDetails

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/GosiPolicyDetailDTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class GosiPolicyDetailDTO
+    {
+        public int GosiPolicyDetailID { get; set; }
+        public int GosiPolicyID { get; set; }
+        public string GosiPolicyDetailCode { get; set; }
+        public string GosiPolicyDetailName { get; set; }
+        public string GosiPolicyDetailAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class GosiPolicyDetailViewModel
+    {
+        public List<GosiPolicyDetailDTO> GosiPolicyDetails { get; set; }
+        public GosiPolicyDetailDTO GosiPolicyDetail { get; set; }
+    }
+}
