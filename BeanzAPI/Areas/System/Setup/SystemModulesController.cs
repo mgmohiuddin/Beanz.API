2b5b138b5be541574816f96fa3be8fda66a0f93e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 23:28:06 +0300

    Lovable: scaffold SystemModulesController.cs for SystemModules

---
+++ BeanzAPI/Areas/System/Setup/SystemModulesController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.System.Setup;
+using Beanz.DTOs.Areas.System.Setup;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.System.Setup
+{
+    [Route("api/[area]/Setup/[controller]/[action]")]
+    [ApiController]
+    [Area("System")]
+    public class SystemModulesController : ControllerBase
+    {
+        private readonly ISystemModuleRepository _systemModulesRepository;
+
+        public SystemModulesController(ISystemModuleRepository systemModulesRepository)
+        {
+            _systemModulesRepository = systemModulesRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<SystemModuleDTO>> GetSystemModules(BeanzCommonDTO common)
+        {
+            var data = await _systemModulesRepository.GetSystemModulesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetSystemModules(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemModulesRepository.SetSystemModulesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostSystemModules(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemModulesRepository.PostSystemModulesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelSystemModules(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemModulesRepository.DelSystemModulesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpSystemModules(BeanzCommonDTO lookup)
+        {
+            var data = await _systemModulesRepository.LookUpSystemModulesAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<SystemModuleViewModel> GetInfoSystemModules(BeanzCommonDTO common)
+        {
+            var data = await _systemModulesRepository.GetInfoSystemModulesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<SystemModuleViewModel> PrintSystemModules(BeanzCommonDTO common)
+        {
+            var data = await _systemModulesRepository.PrintSystemModulesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveSystemModules(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemModulesRepository.ApproveSystemModulesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
