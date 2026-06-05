c1ef7b84ec92fbd51c7a6015b0f9221f9fc740a2
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:57:22 +0300

    Lovable: scaffold IGosiPolicyDetailRepository.cs for GosiPolicyDetails

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IGosiPolicyDetailRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IGosiPolicyDetailRepository
+    {
+        Task<List<GosiPolicyDetailDTO>> GetGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpGosiPolicyDetailsAsync(BeanzCommonDTO lookup);
+        Task<GosiPolicyDetailViewModel> GetInfoGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<GosiPolicyDetailViewModel> PrintGosiPolicyDetailsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveGosiPolicyDetailsAsync(BeanzCommonDTO common);
+    }
+}
