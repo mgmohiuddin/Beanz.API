3cd3789f5f3385f807781a66cfbc197383fc9657
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:59:21 +0300

    Lovable: scaffold IHolidayPolicyPaymentRepository.cs for HolidayPolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IHolidayPolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IHolidayPolicyPaymentRepository
+    {
+        Task<List<HolidayPolicyPaymentDTO>> GetHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpHolidayPolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<HolidayPolicyPaymentViewModel> GetInfoHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<HolidayPolicyPaymentViewModel> PrintHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveHolidayPolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
