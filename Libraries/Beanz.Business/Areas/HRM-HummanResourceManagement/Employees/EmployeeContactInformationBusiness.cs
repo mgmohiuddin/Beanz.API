6c2f353209a07a826e2f2e2f5a75d8455c31ac15
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:15:01 +0300

    Lovable: scaffold EmployeeContactInformationBusiness.cs for EmployeeContactInformations

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeContactInformationBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeContactInformationBusiness
+    {
+        private readonly IEmployeeContactInformationRepository _repository;
+
+        public EmployeeContactInformationBusiness(IEmployeeContactInformationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeContactInformationDTO>> GetEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeContactInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeContactInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeContactInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeContactInformationsAsync(common);
+
+        public Task<EmployeeContactInformationViewModel> GetInfoEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeContactInformationsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeContactInformationsAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeContactInformationsAsync(common);
+    }
+}
