cf0f15d57429ab88f7ef1966e1ea7bb2d9f553d6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:00:21 +0300

    Lovable: scaffold ILeavePolicieRepository.cs for LeavePolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ILeavePolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ILeavePolicieRepository
+    {
+        Task<List<LeavePolicieDTO>> GetLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpLeavePoliciesAsync(BeanzCommonDTO lookup);
+        Task<LeavePolicieViewModel> GetInfoLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<LeavePolicieViewModel> PrintLeavePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveLeavePoliciesAsync(BeanzCommonDTO common);
+    }
+}
