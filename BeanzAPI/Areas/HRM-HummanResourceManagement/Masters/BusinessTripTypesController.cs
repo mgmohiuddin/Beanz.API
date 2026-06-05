ad3dc65ba958de57ff624411f91688e31ded9b7e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 00:04:24 +0300

    Lovable: scaffold BusinessTripTypesController.cs for BusinessTripTypes

---
+++ BeanzAPI/Areas/HRM-HummanResourceManagement/Masters/BusinessTripTypesController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.HummanResourceManagement.Masters
+{
+    [Route("api/[area]/Masters/[controller]")]
+    [ApiController]
+    [Area("HummanResourceManagement")]
+    public class BusinessTripTypesController : ControllerBase
+    {
+        private readonly IBusinessTripTypeRepository _businessTripTypesRepository;
+
+        public BusinessTripTypesController(IBusinessTripTypeRepository businessTripTypesRepository)
+        {
+            _businessTripTypesRepository = businessTripTypesRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<BusinessTripTypeDTO>> GetBusinessTripTypes(BeanzCommonDTO common)
+        {
+            var data = await _businessTripTypesRepository.GetBusinessTripTypesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetBusinessTripTypes(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _businessTripTypesRepository.SetBusinessTripTypesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostBusinessTripTypes(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _businessTripTypesRepository.PostBusinessTripTypesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelBusinessTripTypes(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _businessTripTypesRepository.DelBusinessTripTypesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpBusinessTripTypes(BeanzCommonDTO lookup)
+        {
+            var data = await _businessTripTypesRepository.LookUpBusinessTripTypesAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<BusinessTripTypeViewModel> GetInfoBusinessTripTypes(BeanzCommonDTO common)
+        {
+            var data = await _businessTripTypesRepository.GetInfoBusinessTripTypesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<BusinessTripTypeViewModel> PrintBusinessTripTypes(BeanzCommonDTO common)
+        {
+            var data = await _businessTripTypesRepository.PrintBusinessTripTypesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveBusinessTripTypes(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _businessTripTypesRepository.ApproveBusinessTripTypesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
