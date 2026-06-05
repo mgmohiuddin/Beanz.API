3ac5c93e2c1e05419f9e53236d0b262486217f80
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:01:24 +0300

    Lovable: scaffold ILoanPolicieRepository.cs for LoanPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ILoanPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ILoanPolicieRepository
+    {
+        Task<List<LoanPolicieDTO>> GetLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpLoanPoliciesAsync(BeanzCommonDTO lookup);
+        Task<LoanPolicieViewModel> GetInfoLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<LoanPolicieViewModel> PrintLoanPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveLoanPoliciesAsync(BeanzCommonDTO common);
+    }
+}
