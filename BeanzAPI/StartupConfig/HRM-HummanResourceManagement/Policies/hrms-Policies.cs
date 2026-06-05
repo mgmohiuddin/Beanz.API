e0bb61c7bc0c0ac84f226c093f01327ebec5ec51
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:57:54 +0300

    Lovable: scaffold hrms-Policies.cs (module startup registrations)

--- BeanzAPI/StartupConfig/HRM-HummanResourceManagement/Policies/hrms-Policies.cs
+++ BeanzAPI/StartupConfig/HRM-HummanResourceManagement/Policies/hrms-Policies.cs
@@ -28,6 +28,7 @@ namespace Beanz.API.StartupConfig.HRM_HummanResourceManagement.Policies
             services.AddScoped<IEndOfServicePolicyYearSalarieRepository, EndOfServicePolicyYearSalarieRepository>();
             services.AddScoped<IGosiPolicieRepository, GosiPolicieRepository>();
             services.AddScoped<IGosiPolicyDetailRepository, GosiPolicyDetailRepository>();
+            services.AddScoped<IGosiPolicyEligibleRepository, GosiPolicyEligibleRepository>();
         }
     }
 }
