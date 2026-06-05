e01b2b2ae0b1916ddadd286608820da2ccd0079f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:11:46 +0300

    Lovable: scaffold EmployeeLanguageBusiness.cs for EmployeeLanguages

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeLanguageBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeLanguageBusiness
+    {
+        private readonly IEmployeeLanguageRepository _repository;
+
+        public EmployeeLanguageBusiness(IEmployeeLanguageRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeLanguageDTO>> GetEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeLanguagesAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeLanguagesAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeLanguagesAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeLanguagesAsync(common);
+
+        public Task<EmployeeLanguageViewModel> GetInfoEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeLanguagesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeLanguagesAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeLanguagesAsync(common);
+    }
+}
