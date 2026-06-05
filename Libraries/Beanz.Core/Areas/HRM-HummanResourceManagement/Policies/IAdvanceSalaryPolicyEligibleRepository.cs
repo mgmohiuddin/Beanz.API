157386ba0053336265daa1a514a665310ad317c1
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:44:24 +0300

    Lovable: scaffold IAdvanceSalaryPolicyEligibleRepository.cs for AdvanceSalaryPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAdvanceSalaryPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAdvanceSalaryPolicyEligibleRepository
+    {
+        Task<List<AdvanceSalaryPolicyEligibleDTO>> GetAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<AdvanceSalaryPolicyEligibleViewModel> GetInfoAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<AdvanceSalaryPolicyEligibleViewModel> PrintAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAdvanceSalaryPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
