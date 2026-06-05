50e4038927a46a7ff9c45c98030bfcbac6c4cc9f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:13:00 +0300

    Lovable: scaffold ITrainingPolicieRepository.cs for TrainingPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ITrainingPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ITrainingPolicieRepository
+    {
+        Task<List<TrainingPolicieDTO>> GetTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTrainingPoliciesAsync(BeanzCommonDTO lookup);
+        Task<TrainingPolicieViewModel> GetInfoTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<TrainingPolicieViewModel> PrintTrainingPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTrainingPoliciesAsync(BeanzCommonDTO common);
+    }
+}
