64f318c2d390eccd9e3ccc50d4b3e074140a712f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:55:57 +0300

    Lovable: scaffold EndOfServicePolicyCalculationBusiness.cs for EndOfServicePolicyCalculations

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/EndOfServicePolicyCalculationBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class EndOfServicePolicyCalculationBusiness
+    {
+        private readonly IEndOfServicePolicyCalculationRepository _repository;
+
+        public EndOfServicePolicyCalculationBusiness(IEndOfServicePolicyCalculationRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EndOfServicePolicyCalculationDTO>> GetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.GetEndOfServicePolicyCalculationsAsync(common);
+
+        public Task<BeanzResponseDTO> SetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.SetEndOfServicePolicyCalculationsAsync(common);
+
+        public Task<BeanzResponseDTO> PostEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.PostEndOfServicePolicyCalculationsAsync(common);
+
+        public Task<BeanzResponseDTO> DelEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.DelEndOfServicePolicyCalculationsAsync(common);
+
+        public Task<EndOfServicePolicyCalculationViewModel> GetInfoEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEndOfServicePolicyCalculationsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+            => _repository.ApproveEndOfServicePolicyCalculationsAsync(common);
+    }
+}
