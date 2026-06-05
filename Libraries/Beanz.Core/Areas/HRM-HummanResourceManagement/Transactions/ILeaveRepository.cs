eca466f96765832a578baeba8a441d413e305619
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:19:33 +0300

    Lovable: scaffold ILeaveRepository.cs for Leaves

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Transactions/ILeaveRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Transactions
+{
+    public interface ILeaveRepository
+    {
+        Task<List<LeaveDTO>> GetLeavesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetLeavesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostLeavesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelLeavesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpLeavesAsync(BeanzCommonDTO lookup);
+        Task<LeaveViewModel> GetInfoLeavesAsync(BeanzCommonDTO common);
+        Task<LeaveViewModel> PrintLeavesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveLeavesAsync(BeanzCommonDTO common);
+    }
+}
