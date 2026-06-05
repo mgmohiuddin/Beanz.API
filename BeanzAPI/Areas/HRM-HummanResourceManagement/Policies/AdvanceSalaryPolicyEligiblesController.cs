eddc068d9b1fd1a80a9b82bafee58a14cc2e0529
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:44:22 +0300

    Lovable: scaffold AdvanceSalaryPolicyEligiblesController.cs for AdvanceSalaryPolicyEligibles

---
+++ BeanzAPI/Areas/HRM-HummanResourceManagement/Policies/AdvanceSalaryPolicyEligiblesController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.HummanResourceManagement.Policies
+{
+    [Route("api/[area]/Policies/[controller]/[action]")]
+    [ApiController]
+    [Area("HummanResourceManagement")]
+    public class AdvanceSalaryPolicyEligiblesController : ControllerBase
+    {
+        private readonly IAdvanceSalaryPolicyEligibleRepository _advanceSalaryPolicyEligiblesRepository;
+
+        public AdvanceSalaryPolicyEligiblesController(IAdvanceSalaryPolicyEligibleRepository advanceSalaryPolicyEligiblesRepository)
+        {
+            _advanceSalaryPolicyEligiblesRepository = advanceSalaryPolicyEligiblesRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<AdvanceSalaryPolicyEligibleDTO>> GetAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            var data = await _advanceSalaryPolicyEligiblesRepository.GetAdvanceSalaryPolicyEligiblesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _advanceSalaryPolicyEligiblesRepository.SetAdvanceSalaryPolicyEligiblesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _advanceSalaryPolicyEligiblesRepository.PostAdvanceSalaryPolicyEligiblesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _advanceSalaryPolicyEligiblesRepository.DelAdvanceSalaryPolicyEligiblesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpAdvanceSalaryPolicyEligibles(BeanzCommonDTO lookup)
+        {
+            var data = await _advanceSalaryPolicyEligiblesRepository.LookUpAdvanceSalaryPolicyEligiblesAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<AdvanceSalaryPolicyEligibleViewModel> GetInfoAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            var data = await _advanceSalaryPolicyEligiblesRepository.GetInfoAdvanceSalaryPolicyEligiblesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<AdvanceSalaryPolicyEligibleViewModel> PrintAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            var data = await _advanceSalaryPolicyEligiblesRepository.PrintAdvanceSalaryPolicyEligiblesAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveAdvanceSalaryPolicyEligibles(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _advanceSalaryPolicyEligiblesRepository.ApproveAdvanceSalaryPolicyEligiblesAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
