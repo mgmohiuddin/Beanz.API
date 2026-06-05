3b7fc31ba4d37615e6f98ae83511b85b5f58605b
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:55:54 +0300

    Lovable: scaffold EndOfServicePolicyCalculationDTO.cs for EndOfServicePolicyCalculations

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/EndOfServicePolicyCalculationDTO.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class EndOfServicePolicyCalculationDTO
+    {
+        public int EndOfServicePolicyCalculationID { get; set; }
+        public int EndOfServicePolicyID { get; set; }
+        public string EndOfServicePolicyCalculationCode { get; set; }
+        public string EndOfServicePolicyCalculationName { get; set; }
+        public string EndOfServicePolicyCalculationAlias { get; set; }
+        public int FromYear { get; set; }
+        public int ToYear { get; set; }
+        public decimal Percentage { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class EndOfServicePolicyCalculationViewModel
+    {
+        public List<EndOfServicePolicyCalculationDTO> EndOfServicePolicyCalculations { get; set; }
+        public EndOfServicePolicyCalculationDTO EndOfServicePolicyCalculation { get; set; }
+    }
+}
