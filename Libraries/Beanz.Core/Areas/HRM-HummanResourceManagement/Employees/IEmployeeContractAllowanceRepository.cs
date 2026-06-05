767177e176ed6111c8887a66a54477c716de5d8a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:14:35 +0300

    Lovable: scaffold IEmployeeContractAllowanceRepository.cs for EmployeeContractAllowances

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeContractAllowanceRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeContractAllowanceRepository
+    {
+        Task<List<EmployeeContractAllowanceDTO>> GetEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeContractAllowancesAsync(BeanzCommonDTO lookup);
+        Task<EmployeeContractAllowanceViewModel> GetInfoEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<EmployeeContractAllowanceViewModel> PrintEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeContractAllowancesAsync(BeanzCommonDTO common);
+    }
+}
