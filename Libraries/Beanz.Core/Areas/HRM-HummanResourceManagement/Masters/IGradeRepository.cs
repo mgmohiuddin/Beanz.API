7e9919074c198685e411bfcc3e2052805cfba8ab
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:32:48 +0300

    Lovable: scaffold IGradeRepository.cs for Grades

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IGradeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IGradeRepository
+    {
+        Task<List<GradeDTO>> GetGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelGradesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpGradesAsync(BeanzCommonDTO lookup);
+        Task<GradeViewModel> GetInfoGradesAsync(BeanzCommonDTO common);
+        Task<GradeViewModel> PrintGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveGradesAsync(BeanzCommonDTO common);
+    }
+}
