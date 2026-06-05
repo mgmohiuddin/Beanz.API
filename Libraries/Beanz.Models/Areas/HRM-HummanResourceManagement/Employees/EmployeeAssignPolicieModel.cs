28671faf8206dfa6095ada64e9973ea2c6a4747f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:16:12 +0300

    Lovable: scaffold EmployeeAssignPolicieModel.cs for EmployeeAssignPolicies

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Employees/EmployeeAssignPolicieModel.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Employees.Objects
+{
+    // Entity model generated from table structure
+    public class EmployeeAssignPolicie
+    {
+        public int EmployeeAssignPolicieID { get; set; }
+        public int GosiPolicyID { get; set; }
+        public int OverTimePolicyID { get; set; }
+        public int AttendancePolicyID { get; set; }
+        public int VacationPolicyID { get; set; }
+        public int LeavePolicyID { get; set; }
+        public int ShiftPolicyID { get; set; }
+        public int TicketPolicyID { get; set; }
+        public int LoanPolicyID { get; set; }
+        public int PaidTimeOffPolicyID { get; set; }
+        public int PayrollPolicyID { get; set; }
+        public int AllowancePolicyID { get; set; }
+        public int AdvanceSalaryPolicyID { get; set; }
+        public int BusinessTripPolicyID { get; set; }
+        public int HolidayPolicyID { get; set; }
+        public int EndOfServicePolicyID { get; set; }
+        public int InsurancePolicyID { get; set; }
+        public int TrainingPolicyID { get; set; }
+        public int BonusPolicyID { get; set; }
+        public string EmployeeAssignPolicyCode { get; set; }
+        public string EmployeeAssignPolicyName { get; set; }
+        public string EmployeeAssignPolicyAlias { get; set; }
+        public int EmployeeID { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
