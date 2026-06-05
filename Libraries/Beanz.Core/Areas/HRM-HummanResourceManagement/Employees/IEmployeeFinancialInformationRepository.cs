6782f3129f96999064ed8f9d1ead7f72e3ee9a65
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:12:03 +0300

    Lovable: scaffold IEmployeeFinancialInformationRepository.cs for EmployeeFinancialInformations

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeFinancialInformationRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeFinancialInformationRepository
+    {
+        Task<List<EmployeeFinancialInformationDTO>> GetEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeFinancialInformationsAsync(BeanzCommonDTO lookup);
+        Task<EmployeeFinancialInformationViewModel> GetInfoEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<EmployeeFinancialInformationViewModel> PrintEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeFinancialInformationsAsync(BeanzCommonDTO common);
+    }
+}
