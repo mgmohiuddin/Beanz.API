24427ebe80f022d891142e27eb8b72da19b9c23e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:59:02 +0300

    Lovable: scaffold IHolidayPolicyEligibleRepository.cs for HolidayPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IHolidayPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IHolidayPolicyEligibleRepository
+    {
+        Task<List<HolidayPolicyEligibleDTO>> GetHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpHolidayPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<HolidayPolicyEligibleViewModel> GetInfoHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<HolidayPolicyEligibleViewModel> PrintHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveHolidayPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
