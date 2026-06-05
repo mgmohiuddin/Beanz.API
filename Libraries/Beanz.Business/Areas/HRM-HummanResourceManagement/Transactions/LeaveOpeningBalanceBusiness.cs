5fc4e50ceeac9637cf112cf8854c0f960245f8b0
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:20:00 +0300

    Lovable: scaffold LeaveOpeningBalanceBusiness.cs for LeaveOpeningBalances

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Transactions/LeaveOpeningBalanceBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Transactions
+{
+    public class LeaveOpeningBalanceBusiness
+    {
+        private readonly ILeaveOpeningBalanceRepository _repository;
+
+        public LeaveOpeningBalanceBusiness(ILeaveOpeningBalanceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<LeaveOpeningBalanceDTO>> GetLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.GetLeaveOpeningBalancesAsync(common);
+
+        public Task<BeanzResponseDTO> SetLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.SetLeaveOpeningBalancesAsync(common);
+
+        public Task<BeanzResponseDTO> PostLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.PostLeaveOpeningBalancesAsync(common);
+
+        public Task<BeanzResponseDTO> DelLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.DelLeaveOpeningBalancesAsync(common);
+
+        public Task<LeaveOpeningBalanceViewModel> GetInfoLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoLeaveOpeningBalancesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveLeaveOpeningBalancesAsync(BeanzCommonDTO common)
+            => _repository.ApproveLeaveOpeningBalancesAsync(common);
+    }
+}
