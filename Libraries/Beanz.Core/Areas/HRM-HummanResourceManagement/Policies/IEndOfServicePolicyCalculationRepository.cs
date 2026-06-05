d8d3d7692d4ba70df9010d9d7e3a861e2ef2bb24
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:55:51 +0300

    Lovable: scaffold IEndOfServicePolicyCalculationRepository.cs for EndOfServicePolicyCalculations

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IEndOfServicePolicyCalculationRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IEndOfServicePolicyCalculationRepository
+    {
+        Task<List<EndOfServicePolicyCalculationDTO>> GetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEndOfServicePolicyCalculationsAsync(BeanzCommonDTO lookup);
+        Task<EndOfServicePolicyCalculationViewModel> GetInfoEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<EndOfServicePolicyCalculationViewModel> PrintEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common);
+    }
+}
