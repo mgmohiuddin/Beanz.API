2a4b9768e44275884d8244008b6850feb466e5b8
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:38:18 +0300

    Lovable: scaffold TicketTypeDTO.cs for TicketTypes

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/TicketTypeDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class TicketTypeDTO
+    {
+        public int TicketTypeID { get; set; }
+        public string TicketTypeCode { get; set; }
+        public string TicketTypeName { get; set; }
+        public string TicketTypeAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class TicketTypeViewModel
+    {
+        public List<TicketTypeDTO> TicketTypes { get; set; }
+        public TicketTypeDTO TicketType { get; set; }
+    }
+}
