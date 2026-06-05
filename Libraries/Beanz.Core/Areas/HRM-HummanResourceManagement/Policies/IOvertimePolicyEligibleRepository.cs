d777bec8ee09d2360d9a590227234536dd14ac1e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:08:14 +0300

    Lovable: scaffold IOvertimePolicyEligibleRepository.cs for OvertimePolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IOvertimePolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IOvertimePolicyEligibleRepository
+    {
+        Task<List<OvertimePolicyEligibleDTO>> GetOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpOvertimePolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<OvertimePolicyEligibleViewModel> GetInfoOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<OvertimePolicyEligibleViewModel> PrintOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveOvertimePolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
