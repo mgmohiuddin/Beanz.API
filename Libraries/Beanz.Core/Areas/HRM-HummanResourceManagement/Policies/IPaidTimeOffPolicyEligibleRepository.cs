07fed4d7d80f2e9b25ec62041b16acda05c501b9
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:09:12 +0300

    Lovable: scaffold IPaidTimeOffPolicyEligibleRepository.cs for PaidTimeOffPolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IPaidTimeOffPolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IPaidTimeOffPolicyEligibleRepository
+    {
+        Task<List<PaidTimeOffPolicyEligibleDTO>> GetPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<PaidTimeOffPolicyEligibleViewModel> GetInfoPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<PaidTimeOffPolicyEligibleViewModel> PrintPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
