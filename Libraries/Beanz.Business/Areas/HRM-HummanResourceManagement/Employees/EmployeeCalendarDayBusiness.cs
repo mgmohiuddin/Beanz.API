89d4f0ff2e5c6f5403bbd7cdf93d3ce73040ac46
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:15:56 +0300

    Lovable: scaffold EmployeeCalendarDayBusiness.cs for EmployeeCalendarDays

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Employees/EmployeeCalendarDayBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeCalendarDayBusiness
+    {
+        private readonly IEmployeeCalendarDayRepository _repository;
+
+        public EmployeeCalendarDayBusiness(IEmployeeCalendarDayRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<EmployeeCalendarDayDTO>> GetEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.GetEmployeeCalendarDaysAsync(common);
+
+        public Task<BeanzResponseDTO> SetEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.SetEmployeeCalendarDaysAsync(common);
+
+        public Task<BeanzResponseDTO> PostEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.PostEmployeeCalendarDaysAsync(common);
+
+        public Task<BeanzResponseDTO> DelEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.DelEmployeeCalendarDaysAsync(common);
+
+        public Task<EmployeeCalendarDayViewModel> GetInfoEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.GetInfoEmployeeCalendarDaysAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveEmployeeCalendarDaysAsync(BeanzCommonDTO common)
+            => _repository.ApproveEmployeeCalendarDaysAsync(common);
+    }
+}
