0e52961ea38493a4af03301cbcd7bbfee516cbbe
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:58:42 +0300

    Lovable: scaffold HolidayPolicyDaysController.cs for HolidayPolicyDays

---
+++ BeanzAPI/Areas/HRM-HummanResourceManagement/Policies/HolidayPolicyDaysController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.HummanResourceManagement.Policies
+{
+    [Route("api/[area]/Policies/[controller]/[action]")]
+    [ApiController]
+    [Area("HummanResourceManagement")]
+    public class HolidayPolicyDaysController : ControllerBase
+    {
+        private readonly IHolidayPolicyDayRepository _holidayPolicyDaysRepository;
+
+        public HolidayPolicyDaysController(IHolidayPolicyDayRepository holidayPolicyDaysRepository)
+        {
+            _holidayPolicyDaysRepository = holidayPolicyDaysRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<HolidayPolicyDayDTO>> GetHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            var data = await _holidayPolicyDaysRepository.GetHolidayPolicyDaysAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _holidayPolicyDaysRepository.SetHolidayPolicyDaysAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _holidayPolicyDaysRepository.PostHolidayPolicyDaysAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _holidayPolicyDaysRepository.DelHolidayPolicyDaysAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpHolidayPolicyDays(BeanzCommonDTO lookup)
+        {
+            var data = await _holidayPolicyDaysRepository.LookUpHolidayPolicyDaysAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<HolidayPolicyDayViewModel> GetInfoHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            var data = await _holidayPolicyDaysRepository.GetInfoHolidayPolicyDaysAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<HolidayPolicyDayViewModel> PrintHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            var data = await _holidayPolicyDaysRepository.PrintHolidayPolicyDaysAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveHolidayPolicyDays(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _holidayPolicyDaysRepository.ApproveHolidayPolicyDaysAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
