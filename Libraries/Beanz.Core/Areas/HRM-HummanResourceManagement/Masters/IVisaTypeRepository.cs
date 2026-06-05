f3eb17fa0f7d4428f30d2cd8bf75af50ebbe0136
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:39:46 +0300

    Lovable: scaffold IVisaTypeRepository.cs for VisaTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IVisaTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IVisaTypeRepository
+    {
+        Task<List<VisaTypeDTO>> GetVisaTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetVisaTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostVisaTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelVisaTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpVisaTypesAsync(BeanzCommonDTO lookup);
+        Task<VisaTypeViewModel> GetInfoVisaTypesAsync(BeanzCommonDTO common);
+        Task<VisaTypeViewModel> PrintVisaTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveVisaTypesAsync(BeanzCommonDTO common);
+    }
+}
