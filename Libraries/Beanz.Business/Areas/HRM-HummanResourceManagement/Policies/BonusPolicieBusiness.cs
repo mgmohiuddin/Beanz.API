8934928443f1ecd1dcd62dcc223f953a666c12a3
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:53:31 +0300

    Lovable: scaffold BonusPolicieBusiness.cs for BonusPolicies

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/BonusPolicieBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class BonusPolicieBusiness
+    {
+        private readonly IBonusPolicieRepository _repository;
+
+        public BonusPolicieBusiness(IBonusPolicieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<BonusPolicieDTO>> GetBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetBonusPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> SetBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.SetBonusPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> PostBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.PostBonusPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> DelBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.DelBonusPoliciesAsync(common);
+
+        public Task<BonusPolicieViewModel> GetInfoBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoBonusPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveBonusPoliciesAsync(BeanzCommonDTO common)
+            => _repository.ApproveBonusPoliciesAsync(common);
+    }
+}
