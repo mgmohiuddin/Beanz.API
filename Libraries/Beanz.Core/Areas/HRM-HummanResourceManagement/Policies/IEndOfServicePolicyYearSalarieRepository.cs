079447d45c9afdb18bd757a0e21c44db7287d961
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:56:47 +0300

    Lovable: scaffold IEndOfServicePolicyYearSalarieRepository.cs for EndOfServicePolicyYearSalaries

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IEndOfServicePolicyYearSalarieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IEndOfServicePolicyYearSalarieRepository
+    {
+        Task<List<EndOfServicePolicyYearSalarieDTO>> GetEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO lookup);
+        Task<EndOfServicePolicyYearSalarieViewModel> GetInfoEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<EndOfServicePolicyYearSalarieViewModel> PrintEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEndOfServicePolicyYearSalariesAsync(BeanzCommonDTO common);
+    }
+}
