12332a644c8b331de99e6c7ed0b552c2353850a4
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:58:25 +0300

    Lovable: scaffold HolidayPolicieDTO.cs for HolidayPolicies

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/HolidayPolicieDTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class HolidayPolicieDTO
+    {
+        public int HolidayPolicieID { get; set; }
+        public int HolidayTypeID { get; set; }
+        public string HolidayPolicyCode { get; set; }
+        public string HolidayPolicyName { get; set; }
+        public string HolidayPolicyAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class HolidayPolicieViewModel
+    {
+        public List<HolidayPolicieDTO> HolidayPolicies { get; set; }
+        public HolidayPolicieDTO HolidayPolicie { get; set; }
+    }
+}
