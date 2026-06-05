cb814d07a7ac293f1f17596d84e46e957a42d802
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:38:15 +0300

    Lovable: scaffold ITicketTypeRepository.cs for TicketTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/ITicketTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface ITicketTypeRepository
+    {
+        Task<List<TicketTypeDTO>> GetTicketTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTicketTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTicketTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTicketTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTicketTypesAsync(BeanzCommonDTO lookup);
+        Task<TicketTypeViewModel> GetInfoTicketTypesAsync(BeanzCommonDTO common);
+        Task<TicketTypeViewModel> PrintTicketTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTicketTypesAsync(BeanzCommonDTO common);
+    }
+}
