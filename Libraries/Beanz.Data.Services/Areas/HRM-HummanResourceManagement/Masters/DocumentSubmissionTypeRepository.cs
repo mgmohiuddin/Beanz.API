923d8a482b9f288a68e28e4537f70bdbee93a780
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:52:39 +0300

    Lovable: scaffold DocumentSubmissionTypeRepository.cs for DocumentSubmissionTypes

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Masters/DocumentSubmissionTypeRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Masters;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Masters
+{
+    public class DocumentSubmissionTypeRepository : IDocumentSubmissionTypeRepository
+    {
+        private readonly IMapper _mapper;
+
+        public DocumentSubmissionTypeRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<DocumentSubmissionTypeDTO>> GetDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<DocumentSubmissionTypeDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpDocumentSubmissionTypesAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<DocumentSubmissionTypeViewModel> GetInfoDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoMS_DocumentSubmissionTypes";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _documentSubmissionTypeDTOList = new List<DocumentSubmissionTypeDTO>();
+            bool _isDocumentSubmissionTypesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var documentSubmissionTypeData = objTemp.Where(c => c.TableName == "MS_DocumentSubmissionTypes");
+                        if (documentSubmissionTypeData.Count() > 0 && !_isDocumentSubmissionTypesConsumed)
+                        {
+                            _documentSubmissionTypeDTOList = _mapper.Map<List<DocumentSubmissionTypeDTO>>(documentSubmissionTypeData);
+                            _isDocumentSubmissionTypesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _documentSubmissionTypeDTO = _documentSubmissionTypeDTOList.Where(x => x.DocumentSubmissionTypeID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new DocumentSubmissionTypeViewModel
+                {
+                    DocumentSubmissionTypes = _documentSubmissionTypeDTOList,
+                    DocumentSubmissionType = _documentSubmissionTypeDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<DocumentSubmissionTypeViewModel> PrintDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintMS_DocumentSubmissionTypes";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _documentSubmissionTypeDTOList = new List<DocumentSubmissionTypeDTO>();
+            bool _isDocumentSubmissionTypesConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var documentSubmissionTypeData = objTemp.Where(c => c.TableName == "MS_DocumentSubmissionTypes");
+                        if (documentSubmissionTypeData.Count() > 0 && !_isDocumentSubmissionTypesConsumed)
+                        {
+                            _documentSubmissionTypeDTOList = _mapper.Map<List<DocumentSubmissionTypeDTO>>(documentSubmissionTypeData);
+                            _isDocumentSubmissionTypesConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _documentSubmissionTypeDTO = _documentSubmissionTypeDTOList.Where(x => x.DocumentSubmissionTypeID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new DocumentSubmissionTypeViewModel
+                {
+                    DocumentSubmissionTypes = _documentSubmissionTypeDTOList,
+                    DocumentSubmissionType = _documentSubmissionTypeDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveDocumentSubmissionTypesAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApproveMS_DocumentSubmissionTypes";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@DocumentSubmissionTypeID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
