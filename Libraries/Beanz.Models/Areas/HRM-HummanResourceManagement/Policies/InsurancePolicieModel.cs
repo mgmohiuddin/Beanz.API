94bb6e15e403db0f636b9df670d0d8ad050e7e16
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:59:49 +0300

    Lovable: scaffold InsurancePolicieModel.cs for InsurancePolicies

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Policies/InsurancePolicieModel.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Policies.Objects
+{
+    // Entity model generated from table structure
+    public class InsurancePolicie
+    {
+        public int InsurancePolicieID { get; set; }
+        public int InsuranceTypeID { get; set; }
+        public string InsurancePolicyCode { get; set; }
+        public string InsurancePolicyName { get; set; }
+        public string InsurancePolicyAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
