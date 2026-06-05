85ae2a0b12bfe4ba2d6f9e35919cb0fda14b7f40
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:09:20 +0300

    Lovable: scaffold PaidTimeOffPolicyEligibleBusiness.cs for PaidTimeOffPolicyEligibles

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/PaidTimeOffPolicyEligibleBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class PaidTimeOffPolicyEligibleBusiness
+    {
+        private readonly IPaidTimeOffPolicyEligibleRepository _repository;
+
+        public PaidTimeOffPolicyEligibleBusiness(IPaidTimeOffPolicyEligibleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<PaidTimeOffPolicyEligibleDTO>> GetPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetPaidTimeOffPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> SetPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.SetPaidTimeOffPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> PostPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.PostPaidTimeOffPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> DelPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.DelPaidTimeOffPolicyEligiblesAsync(common);
+
+        public Task<PaidTimeOffPolicyEligibleViewModel> GetInfoPaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoPaidTimeOffPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> ApprovePaidTimeOffPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.ApprovePaidTimeOffPolicyEligiblesAsync(common);
+    }
+}
