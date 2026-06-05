e249c00b9c72e0f0012482f28a11e6b7f29af9d8
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:15:49 +0300

    Lovable: scaffold IEmployeeCalendarDayRepository.cs for EmployeeCalendarDays

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Employees/IEmployeeCalendarDayRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Employees
+{
+    public interface IEmployeeCalendarDayRepository
+    {
+        Task<List<EmployeeCalendarDayDTO>> GetEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEmployeeCalendarDaysAsync(BeanzCommonDTO lookup);
+        Task<EmployeeCalendarDayViewModel> GetInfoEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<EmployeeCalendarDayViewModel> PrintEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEmployeeCalendarDaysAsync(BeanzCommonDTO common);
+    }
+}
