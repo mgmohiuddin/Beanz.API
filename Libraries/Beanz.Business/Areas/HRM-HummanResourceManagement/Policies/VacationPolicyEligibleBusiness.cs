f4ccc720315baf885dd2eb97ed5e8a254f0f2914
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:13:58 +0300

    Lovable: scaffold VacationPolicyEligibleBusiness.cs for VacationPolicyEligibles

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/VacationPolicyEligibleBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class VacationPolicyEligibleBusiness
+    {
+        private readonly IVacationPolicyEligibleRepository _repository;
+
+        public VacationPolicyEligibleBusiness(IVacationPolicyEligibleRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<VacationPolicyEligibleDTO>> GetVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetVacationPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> SetVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.SetVacationPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> PostVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.PostVacationPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> DelVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.DelVacationPolicyEligiblesAsync(common);
+
+        public Task<VacationPolicyEligibleViewModel> GetInfoVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoVacationPolicyEligiblesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveVacationPolicyEligiblesAsync(BeanzCommonDTO common)
+            => _repository.ApproveVacationPolicyEligiblesAsync(common);
+    }
+}
