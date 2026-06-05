65413be601a2ab9317ffc42049bed0c67f46e848
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:58:49 +0300

    Lovable: scaffold HolidayPolicyDayBusiness.cs for HolidayPolicyDays

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/HolidayPolicyDayBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class HolidayPolicyDayBusiness
+    {
+        private readonly IHolidayPolicyDayRepository _repository;
+
+        public HolidayPolicyDayBusiness(IHolidayPolicyDayRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<HolidayPolicyDayDTO>> GetHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.GetHolidayPolicyDaysAsync(common);
+
+        public Task<BeanzResponseDTO> SetHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.SetHolidayPolicyDaysAsync(common);
+
+        public Task<BeanzResponseDTO> PostHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.PostHolidayPolicyDaysAsync(common);
+
+        public Task<BeanzResponseDTO> DelHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.DelHolidayPolicyDaysAsync(common);
+
+        public Task<HolidayPolicyDayViewModel> GetInfoHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.GetInfoHolidayPolicyDaysAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveHolidayPolicyDaysAsync(BeanzCommonDTO common)
+            => _repository.ApproveHolidayPolicyDaysAsync(common);
+    }
+}
