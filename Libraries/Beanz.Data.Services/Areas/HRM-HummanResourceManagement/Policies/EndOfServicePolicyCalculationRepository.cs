8bef6d4f2084b244442ef082777865302af20b0d
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:55:52 +0300

    Lovable: scaffold EndOfServicePolicyCalculationRepository.cs for EndOfServicePolicyCalculations

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Policies/EndOfServicePolicyCalculationRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Policies
+{
+    public class EndOfServicePolicyCalculationRepository : IEndOfServicePolicyCalculationRepository
+    {
+        private readonly IMapper _mapper;
+
+        public EndOfServicePolicyCalculationRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<EndOfServicePolicyCalculationDTO>> GetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<EndOfServicePolicyCalculationDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpEndOfServicePolicyCalculationsAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<EndOfServicePolicyCalculationViewModel> GetInfoEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _endOfServicePolicyCalculationDTOList = new List<EndOfServicePolicyCalculationDTO>();
+            bool _isEndOfServicePolicyCalculationsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var endOfServicePolicyCalculationData = objTemp.Where(c => c.TableName == "POL_EndOfServicePolicyCalculations");
+                        if (endOfServicePolicyCalculationData.Count() > 0 && !_isEndOfServicePolicyCalculationsConsumed)
+                        {
+                            _endOfServicePolicyCalculationDTOList = _mapper.Map<List<EndOfServicePolicyCalculationDTO>>(endOfServicePolicyCalculationData);
+                            _isEndOfServicePolicyCalculationsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _endOfServicePolicyCalculationDTO = _endOfServicePolicyCalculationDTOList.Where(x => x.EndOfServicePolicyCalculationID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new EndOfServicePolicyCalculationViewModel
+                {
+                    EndOfServicePolicyCalculations = _endOfServicePolicyCalculationDTOList,
+                    EndOfServicePolicyCalculation = _endOfServicePolicyCalculationDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<EndOfServicePolicyCalculationViewModel> PrintEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintPOL_EndOfServicePolicyCalculations";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _endOfServicePolicyCalculationDTOList = new List<EndOfServicePolicyCalculationDTO>();
+            bool _isEndOfServicePolicyCalculationsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var endOfServicePolicyCalculationData = objTemp.Where(c => c.TableName == "POL_EndOfServicePolicyCalculations");
+                        if (endOfServicePolicyCalculationData.Count() > 0 && !_isEndOfServicePolicyCalculationsConsumed)
+                        {
+                            _endOfServicePolicyCalculationDTOList = _mapper.Map<List<EndOfServicePolicyCalculationDTO>>(endOfServicePolicyCalculationData);
+                            _isEndOfServicePolicyCalculationsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _endOfServicePolicyCalculationDTO = _endOfServicePolicyCalculationDTOList.Where(x => x.EndOfServicePolicyCalculationID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new EndOfServicePolicyCalculationViewModel
+                {
+                    EndOfServicePolicyCalculations = _endOfServicePolicyCalculationDTOList,
+                    EndOfServicePolicyCalculation = _endOfServicePolicyCalculationDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveEndOfServicePolicyCalculationsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApprovePOL_EndOfServicePolicyCalculations";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EndOfServicePolicyCalculationID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
