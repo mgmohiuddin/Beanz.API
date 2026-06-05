d5d00441cbc820d0a84515437e5c240e15351607
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:50:57 +0300

    Lovable: scaffold DesignationDTO.cs for Designations

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/DesignationDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class DesignationDTO
+    {
+        public int DesignationID { get; set; }
+        public string DesignationCode { get; set; }
+        public string DesignationName { get; set; }
+        public string DesignationAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class DesignationViewModel
+    {
+        public List<DesignationDTO> Designations { get; set; }
+        public DesignationDTO Designation { get; set; }
+    }
+}
