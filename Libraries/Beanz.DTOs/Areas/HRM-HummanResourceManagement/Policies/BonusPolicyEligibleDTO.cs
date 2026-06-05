f83d47ebf2e1f1cb80ff8825162a9a0610353643
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:53:46 +0300

    Lovable: scaffold BonusPolicyEligibleDTO.cs for BonusPolicyEligibles

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/BonusPolicyEligibleDTO.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class BonusPolicyEligibleDTO
+    {
+        public int BonusPolicyEligibleID { get; set; }
+        public int BonusPolicyID { get; set; }
+        public int AreaID { get; set; }
+        public int BranchID { get; set; }
+        public int CityID { get; set; }
+        public int CountryID { get; set; }
+        public int DepartmentID { get; set; }
+        public int DesignationID { get; set; }
+        public int GenderID { get; set; }
+        public int MaritialStatusID { get; set; }
+        public int NationalityID { get; set; }
+        public int OrganizationID { get; set; }
+        public int PositionID { get; set; }
+        public int RegionID { get; set; }
+        public int ReligionID { get; set; }
+        public int SectionID { get; set; }
+        public int SubDepartmentID { get; set; }
+        public int SubGradeID { get; set; }
+        public int SubOrganizationID { get; set; }
+        public int SubSectionID { get; set; }
+        public int GradeID { get; set; }
+        public string BonusPolicyEligibleCode { get; set; }
+        public string BonusPolicyEligibleName { get; set; }
+        public string BonusPolicyEligibleAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class BonusPolicyEligibleViewModel
+    {
+        public List<BonusPolicyEligibleDTO> BonusPolicyEligibles { get; set; }
+        public BonusPolicyEligibleDTO BonusPolicyEligible { get; set; }
+    }
+}
