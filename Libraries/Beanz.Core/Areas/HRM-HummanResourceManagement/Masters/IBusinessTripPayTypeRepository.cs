4a154d896f149a59f51f376149078914a9da9a84
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:31:45 +0300

    Lovable: scaffold IBusinessTripPayTypeRepository.cs for BusinessTripPayTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IBusinessTripPayTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IBusinessTripPayTypeRepository
+    {
+        Task<List<BusinessTripPayTypeDTO>> GetBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpBusinessTripPayTypesAsync(BeanzCommonDTO lookup);
+        Task<BusinessTripPayTypeViewModel> GetInfoBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<BusinessTripPayTypeViewModel> PrintBusinessTripPayTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveBusinessTripPayTypesAsync(BeanzCommonDTO common);
+    }
+}
