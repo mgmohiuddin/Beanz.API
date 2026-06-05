433ef917fbc49016d9000402f7a38f8aa6b574a6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:00:01 +0300

    Lovable: scaffold IInsurancePolicyEligibleRepository.cs for InsurancePolicyEligibles

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IInsurancePolicyEligibleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IInsurancePolicyEligibleRepository
+    {
+        Task<List<InsurancePolicyEligibleDTO>> GetInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpInsurancePolicyEligiblesAsync(BeanzCommonDTO lookup);
+        Task<InsurancePolicyEligibleViewModel> GetInfoInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<InsurancePolicyEligibleViewModel> PrintInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveInsurancePolicyEligiblesAsync(BeanzCommonDTO common);
+    }
+}
