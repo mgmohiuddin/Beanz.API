6dcdfa23f31ccddc80ba2278f2b56ccd735ca6a6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:39:29 +0300

    Lovable: scaffold IVacationTypeRepository.cs for VacationTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IVacationTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IVacationTypeRepository
+    {
+        Task<List<VacationTypeDTO>> GetVacationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetVacationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostVacationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelVacationTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpVacationTypesAsync(BeanzCommonDTO lookup);
+        Task<VacationTypeViewModel> GetInfoVacationTypesAsync(BeanzCommonDTO common);
+        Task<VacationTypeViewModel> PrintVacationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveVacationTypesAsync(BeanzCommonDTO common);
+    }
+}
