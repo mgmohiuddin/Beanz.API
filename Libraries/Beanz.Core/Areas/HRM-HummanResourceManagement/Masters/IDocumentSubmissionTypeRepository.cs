0b72e1895dee46ba495867b79915b509d8849511
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 11:52:37 +0300

    Lovable: scaffold IDocumentSubmissionTypeRepository.cs for DocumentSubmissionTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IDocumentSubmissionTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IDocumentSubmissionTypeRepository
+    {
+        Task<List<DocumentSubmissionTypeDTO>> GetDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpDocumentSubmissionTypesAsync(BeanzCommonDTO lookup);
+        Task<DocumentSubmissionTypeViewModel> GetInfoDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<DocumentSubmissionTypeViewModel> PrintDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveDocumentSubmissionTypesAsync(BeanzCommonDTO common);
+    }
+}
