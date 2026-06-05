1f85915d73421651277666e623ac3073426b6bad
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:33:10 +0300

    Lovable: scaffold HolidayTypeModel.cs for HolidayTypes

---
+++ Libraries/Beanz.Models/Areas/HRM-HummanResourceManagement/Masters/HolidayTypeModel.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Beanz.Models.Areas.HummanResourceManagement.Masters.Objects
+{
+    // Entity model generated from table structure
+    public class HolidayType
+    {
+        public int HolidayTypeID { get; set; }
+        public string HolidayTypeCode { get; set; }
+        public string HolidayTypeName { get; set; }
+        public string HolidayTypeAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
