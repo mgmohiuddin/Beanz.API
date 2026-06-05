4f70f6af8ecea62e8e574769bdf8253460e1b127
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:09:55 +0300

    Lovable: scaffold IPayrollPolicieRepository.cs for PayrollPolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IPayrollPolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IPayrollPolicieRepository
+    {
+        Task<List<PayrollPolicieDTO>> GetPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPayrollPoliciesAsync(BeanzCommonDTO lookup);
+        Task<PayrollPolicieViewModel> GetInfoPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<PayrollPolicieViewModel> PrintPayrollPoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePayrollPoliciesAsync(BeanzCommonDTO common);
+    }
+}
