e08feb35a9cb72dc7b03391c6e7dec2ef4218a3e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:38:37 +0300

    Lovable: scaffold ITrainingTypeRepository.cs for TrainingTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/ITrainingTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface ITrainingTypeRepository
+    {
+        Task<List<TrainingTypeDTO>> GetTrainingTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTrainingTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTrainingTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTrainingTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTrainingTypesAsync(BeanzCommonDTO lookup);
+        Task<TrainingTypeViewModel> GetInfoTrainingTypesAsync(BeanzCommonDTO common);
+        Task<TrainingTypeViewModel> PrintTrainingTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTrainingTypesAsync(BeanzCommonDTO common);
+    }
+}
