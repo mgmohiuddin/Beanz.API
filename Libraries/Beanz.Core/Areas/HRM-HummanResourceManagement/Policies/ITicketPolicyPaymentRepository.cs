67aefa2f07ffa185c3cb36f79178c1d036971f43
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:12:41 +0300

    Lovable: scaffold ITicketPolicyPaymentRepository.cs for TicketPolicyPayments

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/ITicketPolicyPaymentRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface ITicketPolicyPaymentRepository
+    {
+        Task<List<TicketPolicyPaymentDTO>> GetTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTicketPolicyPaymentsAsync(BeanzCommonDTO lookup);
+        Task<TicketPolicyPaymentViewModel> GetInfoTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<TicketPolicyPaymentViewModel> PrintTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTicketPolicyPaymentsAsync(BeanzCommonDTO common);
+    }
+}
