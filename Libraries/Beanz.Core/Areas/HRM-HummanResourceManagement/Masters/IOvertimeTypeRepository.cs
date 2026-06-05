7f8aca343a33cb3879e35c947f66d6b221bed543
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:34:15 +0300

    Lovable: scaffold IOvertimeTypeRepository.cs for OvertimeTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IOvertimeTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IOvertimeTypeRepository
+    {
+        Task<List<OvertimeTypeDTO>> GetOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpOvertimeTypesAsync(BeanzCommonDTO lookup);
+        Task<OvertimeTypeViewModel> GetInfoOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<OvertimeTypeViewModel> PrintOvertimeTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveOvertimeTypesAsync(BeanzCommonDTO common);
+    }
+}
