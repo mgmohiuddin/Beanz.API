e51adc29cb5f2d372ac0459558b91591d03f3dd9
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:06:48 +0300

    Lovable: scaffold IOverTimePolicieRepository.cs for OverTimePolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IOverTimePolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IOverTimePolicieRepository
+    {
+        Task<List<OverTimePolicieDTO>> GetOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpOverTimePoliciesAsync(BeanzCommonDTO lookup);
+        Task<OverTimePolicieViewModel> GetInfoOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<OverTimePolicieViewModel> PrintOverTimePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveOverTimePoliciesAsync(BeanzCommonDTO common);
+    }
+}
