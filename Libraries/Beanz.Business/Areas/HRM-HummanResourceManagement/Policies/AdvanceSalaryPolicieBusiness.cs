a940650339c8013e0d7b73d46733ac1fed1f7895
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:43:36 +0300

    Lovable: scaffold AdvanceSalaryPolicieBusiness.cs for AdvanceSalaryPolicies

---
+++ Libraries/Beanz.Business/Areas/HRM-HummanResourceManagement/Policies/AdvanceSalaryPolicieBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.HummanResourceManagement.Policies
+{
+    public class AdvanceSalaryPolicieBusiness
+    {
+        private readonly IAdvanceSalaryPolicieRepository _repository;
+
+        public AdvanceSalaryPolicieBusiness(IAdvanceSalaryPolicieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<AdvanceSalaryPolicieDTO>> GetAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetAdvanceSalaryPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> SetAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.SetAdvanceSalaryPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> PostAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.PostAdvanceSalaryPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> DelAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.DelAdvanceSalaryPoliciesAsync(common);
+
+        public Task<AdvanceSalaryPolicieViewModel> GetInfoAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.GetInfoAdvanceSalaryPoliciesAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveAdvanceSalaryPoliciesAsync(BeanzCommonDTO common)
+            => _repository.ApproveAdvanceSalaryPoliciesAsync(common);
+    }
+}
