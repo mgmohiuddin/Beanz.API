69ee0374a9e433d9cdf34d644a00ad045b6ec48f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:31:48 +0300

    Lovable: scaffold IEducationTypeRepository.cs for EducationTypes

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Masters/IEducationTypeRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Masters
+{
+    public interface IEducationTypeRepository
+    {
+        Task<List<EducationTypeDTO>> GetEducationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetEducationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostEducationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelEducationTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpEducationTypesAsync(BeanzCommonDTO lookup);
+        Task<EducationTypeViewModel> GetInfoEducationTypesAsync(BeanzCommonDTO common);
+        Task<EducationTypeViewModel> PrintEducationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveEducationTypesAsync(BeanzCommonDTO common);
+    }
+}
