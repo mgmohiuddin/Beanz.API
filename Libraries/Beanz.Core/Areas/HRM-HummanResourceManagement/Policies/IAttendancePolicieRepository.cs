140af68675bf77c9bc8de088cf1f34a77532b5b6
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:52:10 +0300

    Lovable: scaffold IAttendancePolicieRepository.cs for AttendancePolicies

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Policies/IAttendancePolicieRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Policies;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Policies
+{
+    public interface IAttendancePolicieRepository
+    {
+        Task<List<AttendancePolicieDTO>> GetAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpAttendancePoliciesAsync(BeanzCommonDTO lookup);
+        Task<AttendancePolicieViewModel> GetInfoAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<AttendancePolicieViewModel> PrintAttendancePoliciesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveAttendancePoliciesAsync(BeanzCommonDTO common);
+    }
+}
