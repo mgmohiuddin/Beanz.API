f57933878ee8e1b9e7254da09840f0d230608e3a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:08:00 +0300

    Lovable: scaffold OverTimePolicyAllowanceDTO.cs for OverTimePolicyAllowance

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/OverTimePolicyAllowanceDTO.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class OverTimePolicyAllowanceDTO
+    {
+        public int OverTimePolicyAllowanceID { get; set; }
+        public int AllowanceID { get; set; }
+        public int OverTimePolicyID { get; set; }
+        public string OverTimePolicyAllowanceCode { get; set; }
+        public string OverTimePolicyAllowanceName { get; set; }
+        public string OverTimePolicyAllowanceAlias { get; set; }
+        public decimal HolidayRate { get; set; }
+        public decimal WeekDaysRate { get; set; }
+        public decimal WeekEndRate { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class OverTimePolicyAllowanceViewModel
+    {
+        public List<OverTimePolicyAllowanceDTO> OverTimePolicyAllowances { get; set; }
+        public OverTimePolicyAllowanceDTO OverTimePolicyAllowance { get; set; }
+    }
+}
