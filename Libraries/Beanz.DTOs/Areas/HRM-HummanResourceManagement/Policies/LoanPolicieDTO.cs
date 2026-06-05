1c518200f42863df0309a2504a4c93217ac8f891
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:01:27 +0300

    Lovable: scaffold LoanPolicieDTO.cs for LoanPolicies

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Policies/LoanPolicieDTO.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Policies
+{
+    public class LoanPolicieDTO
+    {
+        public int LoanPolicieID { get; set; }
+        public int LoanTypeID { get; set; }
+        public string LoanPolicyCode { get; set; }
+        public string LoanPolicyName { get; set; }
+        public string LoanPolicyAlias { get; set; }
+        public int MinLoan { get; set; }
+        public int MaxLoan { get; set; }
+        public int NoOfEMIs { get; set; }
+        public int MaxNoOfEMIs { get; set; }
+        public int MinWorkYears { get; set; }
+        public int MaxDeductionPercentage { get; set; }
+        public bool IsValueFromEOS { get; set; }
+        public bool IsAllowEMIReshedule { get; set; }
+        public bool IsGuarentorRequired { get; set; }
+        public int LoanIntervalDays { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class LoanPolicieViewModel
+    {
+        public List<LoanPolicieDTO> LoanPolicies { get; set; }
+        public LoanPolicieDTO LoanPolicie { get; set; }
+    }
+}
