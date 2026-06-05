b216809771952adab32abdbcd2f397e5983e572e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 23:28:29 +0300

    Lovable: scaffold SystemScreenEndpointBusiness.cs for SystemScreenEndpoints

---
+++ Libraries/Beanz.Business/Areas/System/Setup/SystemScreenEndpointBusiness.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.Core.Areas.System.Setup;
+using Beanz.DTOs.Areas.System.Setup;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.Business.Areas.System.Setup
+{
+    public class SystemScreenEndpointBusiness
+    {
+        private readonly ISystemScreenEndpointRepository _repository;
+
+        public SystemScreenEndpointBusiness(ISystemScreenEndpointRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<SystemScreenEndpointDTO>> GetSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.GetSystemScreenEndpointsAsync(common);
+
+        public Task<BeanzResponseDTO> SetSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.SetSystemScreenEndpointsAsync(common);
+
+        public Task<BeanzResponseDTO> PostSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.PostSystemScreenEndpointsAsync(common);
+
+        public Task<BeanzResponseDTO> DelSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.DelSystemScreenEndpointsAsync(common);
+
+        public Task<SystemScreenEndpointViewModel> GetInfoSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.GetInfoSystemScreenEndpointsAsync(common);
+
+        public Task<BeanzResponseDTO> ApproveSystemScreenEndpointsAsync(BeanzCommonDTO common)
+            => _repository.ApproveSystemScreenEndpointsAsync(common);
+    }
+}
