8cc45f68f2e12c6e99f9d27c2c904c3369355e40
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:19:37 +0300

    Lovable: scaffold LeaveDTO.cs for Leaves

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Transactions/LeaveDTO.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Transactions
+{
+    public class LeaveDTO
+    {
+        public int LeaveID { get; set; }
+        public int LeavePolicyID { get; set; }
+        public int LeaveTypeID { get; set; }
+        public int LeaveStatusID { get; set; }
+        public int LeaveRequisitionID { get; set; }
+        public string LeaveCode { get; set; }
+        public DateTime LeaveDate { get; set; }
+        public int EmployeeID { get; set; }
+        public DateTime LeaveStartDate { get; set; }
+        public DateTime LeaveEndDate { get; set; }
+        public int TotalDays { get; set; }
+        public int PaidDays { get; set; }
+        public int UnPaidDays { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class LeaveViewModel
+    {
+        public List<LeaveDTO> Leaves { get; set; }
+        public LeaveDTO Leave { get; set; }
+    }
+}
