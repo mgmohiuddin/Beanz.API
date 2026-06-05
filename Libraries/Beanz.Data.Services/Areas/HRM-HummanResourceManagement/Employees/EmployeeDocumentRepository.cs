b246b7d907dff2bd43c3a658c4df35ab0287198e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:12:44 +0300

    Lovable: scaffold EmployeeDocumentRepository.cs for EmployeeDocuments

---
+++ Libraries/Beanz.Data.Services/Areas/HRM-HummanResourceManagement/Employees/EmployeeDocumentRepository.cs
@@ -0,0 +1,225 @@
+using AutoMapper;
+using Beanz.Core.Areas.HummanResourceManagement.Employees;
+using Beanz.Data.Services.DataAccessLayer;
+using Beanz.Data.Services.DataAccessLayer.DAL;
+using Beanz.DTOs.Areas;
+using Beanz.DTOs.Areas.HummanResourceManagement.Employees;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Beanz.Data.Services.Areas.HummanResourceManagement.Employees
+{
+    public class EmployeeDocumentRepository : IEmployeeDocumentRepository
+    {
+        private readonly IMapper _mapper;
+
+        public EmployeeDocumentRepository(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<List<EmployeeDocumentDTO>> GetEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<EmployeeDocumentDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<BeanzResponseDTO> SetEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.SetEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@Json", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> PostEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PostEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<BeanzResponseDTO> DelEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.DelEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+
+        public async Task<List<BeanzlookupDTO>> LookUpEmployeeDocumentsAsync(BeanzCommonDTO lookup)
+        {
+            string _sql = "hrms.LookupEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = lookup.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = lookup.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = lookup.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = lookup.LanguageID },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = lookup.PrimaryKeyID }
+            };
+            var data = await DAL.GetObjectListWithParametersAsync<BeanzlookupDTO>(_sql, _parameters);
+            return data;
+        }
+
+        public async Task<EmployeeDocumentViewModel> GetInfoEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.GetInfoEmployeeDocuments";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _employeeDocumentDTOList = new List<EmployeeDocumentDTO>();
+            bool _isEmployeeDocumentsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var employeeDocumentData = objTemp.Where(c => c.TableName == "EmployeeDocuments");
+                        if (employeeDocumentData.Count() > 0 && !_isEmployeeDocumentsConsumed)
+                        {
+                            _employeeDocumentDTOList = _mapper.Map<List<EmployeeDocumentDTO>>(employeeDocumentData);
+                            _isEmployeeDocumentsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _employeeDocumentDTO = _employeeDocumentDTOList.Where(x => x.EmployeeDocumentID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new EmployeeDocumentViewModel
+                {
+                    EmployeeDocuments = _employeeDocumentDTOList,
+                    EmployeeDocument = _employeeDocumentDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<EmployeeDocumentViewModel> PrintEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.PrintEmployeeDocuments";
+            SqlParameter[] paramList =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@JSon", SqlDbType.NVarChar) { Value = common.Json },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID }
+            };
+
+            DynamicParameters p = new DynamicParameters();
+            foreach (var param in paramList)
+            {
+                p.Add(param.ParameterName, param.Value);
+            }
+            var _employeeDocumentDTOList = new List<EmployeeDocumentDTO>();
+            bool _isEmployeeDocumentsConsumed = false;
+            using (SqlConnection _sqlConnection = new SqlConnection(Utilities.Configuration.ConnectionString))
+            {
+                using (var objRecord = await SqlMapper.QueryMultipleAsync(_sqlConnection, _sql, p, commandType: CommandType.StoredProcedure))
+                {
+                    while (!objRecord.IsConsumed)
+                    {
+                        var objTemp = objRecord.Read<dynamic>();
+                        var employeeDocumentData = objTemp.Where(c => c.TableName == "EmployeeDocuments");
+                        if (employeeDocumentData.Count() > 0 && !_isEmployeeDocumentsConsumed)
+                        {
+                            _employeeDocumentDTOList = _mapper.Map<List<EmployeeDocumentDTO>>(employeeDocumentData);
+                            _isEmployeeDocumentsConsumed = true;
+                            continue;
+                        }
+                    }
+                }
+                var _employeeDocumentDTO = _employeeDocumentDTOList.Where(x => x.EmployeeDocumentID == common.PrimaryKeyID).FirstOrDefault();
+                var result = new EmployeeDocumentViewModel
+                {
+                    EmployeeDocuments = _employeeDocumentDTOList,
+                    EmployeeDocument = _employeeDocumentDTO
+                };
+                return result;
+            }
+        }
+
+        public async Task<BeanzResponseDTO> ApproveEmployeeDocumentsAsync(BeanzCommonDTO common)
+        {
+            string _sql = "hrms.ApproveEmployeeDocuments";
+            SqlParameter[] _parameters =
+            {
+                new SqlParameter("@CompanyID", SqlDbType.Int) { Value = common.CompanyID },
+                new SqlParameter("@UserID", SqlDbType.Int) { Value = common.UserID },
+                new SqlParameter("@Type", SqlDbType.Int) { Value = common.Type },
+                new SqlParameter("@LanguageID", SqlDbType.Int) { Value = common.LanguageID },
+                new SqlParameter("@EmployeeDocumentID", SqlDbType.Int) { Value = common.PrimaryKeyID },
+                new SqlParameter("@ResponseID", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ResponseMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorCode", SqlDbType.NVarChar) { Direction = ParameterDirection.Output },
+                new SqlParameter("@ErrorMessage", SqlDbType.NVarChar) { Direction = ParameterDirection.Output }
+            };
+            return await SQLDataAccessLayer.MultipleOutputBySpAsync<BeanzResponseDTO>(_sql, _parameters);
+        }
+    }
+}
