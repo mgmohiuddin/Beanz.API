8e574409fcb8a5fd8db695c05c12284d55804925
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 14:38:41 +0300

    Lovable: scaffold TrainingTypeDTO.cs for TrainingTypes

---
+++ Libraries/Beanz.DTOs/Areas/HRM-HummanResourceManagement/Masters/TrainingTypeDTO.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.HummanResourceManagement.Masters
+{
+    public class TrainingTypeDTO
+    {
+        public int TrainingTypeID { get; set; }
+        public string TrainingTypeCode { get; set; }
+        public string TrainingTypeName { get; set; }
+        public string TrainingTypeAlias { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class TrainingTypeViewModel
+    {
+        public List<TrainingTypeDTO> TrainingTypes { get; set; }
+        public TrainingTypeDTO TrainingType { get; set; }
+    }
+}
