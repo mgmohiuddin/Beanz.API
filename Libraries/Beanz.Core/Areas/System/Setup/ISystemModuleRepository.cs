d98ec6319e3cf09042f70101385ccd11a5fd7185
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 23:28:08 +0300

    Lovable: scaffold ISystemModuleRepository.cs for SystemModules

---
+++ Libraries/Beanz.Core/Areas/System/Setup/ISystemModuleRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.System.Setup;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.System.Setup
+{
+    public interface ISystemModuleRepository
+    {
+        Task<List<SystemModuleDTO>> GetSystemModulesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetSystemModulesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostSystemModulesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelSystemModulesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpSystemModulesAsync(BeanzCommonDTO lookup);
+        Task<SystemModuleViewModel> GetInfoSystemModulesAsync(BeanzCommonDTO common);
+        Task<SystemModuleViewModel> PrintSystemModulesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveSystemModulesAsync(BeanzCommonDTO common);
+    }
+}
