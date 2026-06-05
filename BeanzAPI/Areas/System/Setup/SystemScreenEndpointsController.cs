9c5e6c54f1f74ecce75f88eceb93db73e7546cdd
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 23:28:21 +0300

    Lovable: scaffold SystemScreenEndpointsController.cs for SystemScreenEndpoints

---
+++ BeanzAPI/Areas/System/Setup/SystemScreenEndpointsController.cs
@@ -0,0 +1,118 @@
+using Beanz.Core.Areas.System.Setup;
+using Beanz.DTOs.Areas.System.Setup;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beanz.API.Areas.System.Setup
+{
+    [Route("api/[area]/Setup/[controller]/[action]")]
+    [ApiController]
+    [Area("System")]
+    public class SystemScreenEndpointsController : ControllerBase
+    {
+        private readonly ISystemScreenEndpointRepository _systemScreenEndpointsRepository;
+
+        public SystemScreenEndpointsController(ISystemScreenEndpointRepository systemScreenEndpointsRepository)
+        {
+            _systemScreenEndpointsRepository = systemScreenEndpointsRepository;
+        }
+
+        [HttpPost("Get")]
+        public async Task<List<SystemScreenEndpointDTO>> GetSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            var data = await _systemScreenEndpointsRepository.GetSystemScreenEndpointsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Set")]
+        public async Task<ActionResult> SetSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemScreenEndpointsRepository.SetSystemScreenEndpointsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Post")]
+        public async Task<ActionResult> PostSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemScreenEndpointsRepository.PostSystemScreenEndpointsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("Del")]
+        public async Task<ActionResult> DelSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemScreenEndpointsRepository.DelSystemScreenEndpointsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+
+        [HttpPost("LookUp")]
+        public async Task<List<BeanzlookupDTO>> LookUpSystemScreenEndpoints(BeanzCommonDTO lookup)
+        {
+            var data = await _systemScreenEndpointsRepository.LookUpSystemScreenEndpointsAsync(lookup);
+            return data;
+        }
+
+        [HttpPost("GetInfo")]
+        public async Task<SystemScreenEndpointViewModel> GetInfoSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            var data = await _systemScreenEndpointsRepository.GetInfoSystemScreenEndpointsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Print")]
+        public async Task<SystemScreenEndpointViewModel> PrintSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            var data = await _systemScreenEndpointsRepository.PrintSystemScreenEndpointsAsync(common);
+            return data;
+        }
+
+        [HttpPost("Approve")]
+        public async Task<ActionResult> ApproveSystemScreenEndpoints(BeanzCommonDTO common)
+        {
+            try
+            {
+                BeanzResponseDTO beanzResponseDTO = await _systemScreenEndpointsRepository.ApproveSystemScreenEndpointsAsync(common);
+                if (beanzResponseDTO.ErrorCode != "")
+                    return BadRequest(beanzResponseDTO);
+                else
+                    return Ok(beanzResponseDTO);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }
+        }
+    }
+}
