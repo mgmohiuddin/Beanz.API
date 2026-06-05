4da8f4149c9e55f48767d1ebd12b3f12042d900a
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 16:20:09 +0300

    Lovable: scaffold ILeaveDayRepository.cs for LeaveDays

---
+++ Libraries/Beanz.Core/Areas/HRM-HummanResourceManagement/Transactions/ILeaveDayRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.HummanResourceManagement.Transactions;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.HummanResourceManagement.Transactions
+{
+    public interface ILeaveDayRepository
+    {
+        Task<List<LeaveDayDTO>> GetLeaveDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetLeaveDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostLeaveDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelLeaveDaysAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpLeaveDaysAsync(BeanzCommonDTO lookup);
+        Task<LeaveDayViewModel> GetInfoLeaveDaysAsync(BeanzCommonDTO common);
+        Task<LeaveDayViewModel> PrintLeaveDaysAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveLeaveDaysAsync(BeanzCommonDTO common);
+    }
+}
