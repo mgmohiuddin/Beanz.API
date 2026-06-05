47a34eaa92e1152f78506dbde92759fcde182574
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-05 15:15:18 +0300

    Lovable: scaffold hrms-Statuses.cs (module startup registrations)

---
+++ BeanzAPI/StartupConfig/HRM-HummanResourceManagement/Statuses/hrms-Statuses.cs
@@ -0,0 +1,13 @@
+using Beanz.Core.Areas.HummanResourceManagement.Statuses;
+using Beanz.Data.Services.Areas.HummanResourceManagement.Statuses;
+
+namespace Beanz.API.StartupConfig.HRM_HummanResourceManagement.Statuses
+{
+    public class hrms_Statuses
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<IEmployeeContractStatuseRepository, EmployeeContractStatuseRepository>();
+        }
+    }
+}
