695b33227e5842fb5b24489ad4a4e2d885391377
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:53:14 +0300

    Lovable: scaffold ISubGradeBusiness.cs for SubGrades

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/ISubGradeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface ISubGradeBusiness
+    {
+        Task<List<SubGradeDTO>> GetSubGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetSubGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostSubGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelSubGradesAsync(BeanzCommonDTO common);
+        Task<SubGradeViewModel> GetInfoSubGradesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpSubGradesAsync(BeanzCommonDTO lookup);
+        Task<SubGradeViewModel> PrintSubGradesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveSubGradesAsync(BeanzCommonDTO common);
+    }
+}
