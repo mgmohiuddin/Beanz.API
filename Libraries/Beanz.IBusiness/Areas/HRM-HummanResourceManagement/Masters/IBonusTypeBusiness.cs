20f75c4ff472d67b4a557e04bdb99b792acfc3fd
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:44:05 +0300

    Lovable: scaffold IBonusTypeBusiness.cs for BonusTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IBonusTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IBonusTypeBusiness
+    {
+        Task<List<BonusTypeDTO>> GetBonusTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetBonusTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostBonusTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelBonusTypesAsync(BeanzCommonDTO common);
+        Task<BonusTypeViewModel> GetInfoBonusTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpBonusTypesAsync(BeanzCommonDTO lookup);
+        Task<BonusTypeViewModel> PrintBonusTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveBonusTypesAsync(BeanzCommonDTO common);
+    }
+}
