9a3ef64accf4d707901d168bdf59f41252608ebd
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:48:27 +0300

    Lovable: scaffold IPayrollTypeBusiness.cs for PayrollTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IPayrollTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IPayrollTypeBusiness
+    {
+        Task<List<PayrollTypeDTO>> GetPayrollTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPayrollTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPayrollTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPayrollTypesAsync(BeanzCommonDTO common);
+        Task<PayrollTypeViewModel> GetInfoPayrollTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPayrollTypesAsync(BeanzCommonDTO lookup);
+        Task<PayrollTypeViewModel> PrintPayrollTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePayrollTypesAsync(BeanzCommonDTO common);
+    }
+}
