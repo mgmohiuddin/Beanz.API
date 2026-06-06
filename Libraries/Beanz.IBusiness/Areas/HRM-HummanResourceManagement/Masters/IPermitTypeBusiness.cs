19f386245c2f45b7e175e0ef1e44fb082ecf8c2f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:48:41 +0300

    Lovable: scaffold IPermitTypeBusiness.cs for PermitTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IPermitTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IPermitTypeBusiness
+    {
+        Task<List<PermitTypeDTO>> GetPermitTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPermitTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPermitTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPermitTypesAsync(BeanzCommonDTO common);
+        Task<PermitTypeViewModel> GetInfoPermitTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPermitTypesAsync(BeanzCommonDTO lookup);
+        Task<PermitTypeViewModel> PrintPermitTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePermitTypesAsync(BeanzCommonDTO common);
+    }
+}
