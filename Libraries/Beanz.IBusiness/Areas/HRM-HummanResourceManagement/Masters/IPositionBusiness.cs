dc854bb7c893bb9bfdd03cd2c145a2cca9ede422
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:48:56 +0300

    Lovable: scaffold IPositionBusiness.cs for Positions

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IPositionBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IPositionBusiness
+    {
+        Task<List<PositionDTO>> GetPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPositionsAsync(BeanzCommonDTO common);
+        Task<PositionViewModel> GetInfoPositionsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPositionsAsync(BeanzCommonDTO lookup);
+        Task<PositionViewModel> PrintPositionsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePositionsAsync(BeanzCommonDTO common);
+    }
+}
