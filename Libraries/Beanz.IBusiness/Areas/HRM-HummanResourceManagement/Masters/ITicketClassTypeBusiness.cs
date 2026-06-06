3b62f8cd9880e53f38756249a569a39fd0303139
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:53:29 +0300

    Lovable: scaffold ITicketClassTypeBusiness.cs for TicketClassTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/ITicketClassTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface ITicketClassTypeBusiness
+    {
+        Task<List<TicketClassTypeDTO>> GetTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<TicketClassTypeViewModel> GetInfoTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTicketClassTypesAsync(BeanzCommonDTO lookup);
+        Task<TicketClassTypeViewModel> PrintTicketClassTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTicketClassTypesAsync(BeanzCommonDTO common);
+    }
+}
