7870e9a0adcfa5d133f88597781b659f1776b34d
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:54:11 +0300

    Lovable: scaffold ITravelTypeBusiness.cs for TravelTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/ITravelTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface ITravelTypeBusiness
+    {
+        Task<List<TravelTypeDTO>> GetTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelTravelTypesAsync(BeanzCommonDTO common);
+        Task<TravelTypeViewModel> GetInfoTravelTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpTravelTypesAsync(BeanzCommonDTO lookup);
+        Task<TravelTypeViewModel> PrintTravelTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveTravelTypesAsync(BeanzCommonDTO common);
+    }
+}
