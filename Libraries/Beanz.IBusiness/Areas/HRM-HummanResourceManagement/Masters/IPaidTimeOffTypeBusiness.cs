bfa61b1306bf55fc060cb5d7fbacef2c246c713f
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:48:13 +0300

    Lovable: scaffold IPaidTimeOffTypeBusiness.cs for PaidTimeOffTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IPaidTimeOffTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IPaidTimeOffTypeBusiness
+    {
+        Task<List<PaidTimeOffTypeDTO>> GetPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<PaidTimeOffTypeViewModel> GetInfoPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpPaidTimeOffTypesAsync(BeanzCommonDTO lookup);
+        Task<PaidTimeOffTypeViewModel> PrintPaidTimeOffTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApprovePaidTimeOffTypesAsync(BeanzCommonDTO common);
+    }
+}
