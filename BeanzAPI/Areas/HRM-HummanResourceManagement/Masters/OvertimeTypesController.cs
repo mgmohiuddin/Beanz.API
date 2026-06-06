5d38b8e2cc12fe2e392654d98be3dfdb400e185e
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:47:51 +0300

    Lovable: scaffold OvertimeTypesController.cs for OvertimeTypes

--- BeanzAPI/Areas/HRM-HummanResourceManagement/Masters/OvertimeTypesController.cs
+++ BeanzAPI/Areas/HRM-HummanResourceManagement/Masters/OvertimeTypesController.cs
@@ -3,26 +3,27 @@ using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
 using Beanz.DTOs.BeanzCommon;
 using Beanz.DTOs.BeanzRoutes;
 using Beanz.DTOs.Common;
+using Beanz.IBusiness.Areas.HummanResourceManagement.Masters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Beanz.API.Areas.HummanResourceManagement.Masters
 {
-    [Route("api/[area]/Masters/[controller]/[action]")]
+    [Route("api/[area]/Masters/[controller]")]
     [ApiController]
     [Area("HummanResourceManagement")]
     public class OvertimeTypesController : ControllerBase
     {
-        private readonly IOvertimeTypeRepository _overtimeTypesRepository;
+        private readonly IOvertimeTypeBusiness _overtimeTypesBusiness;
 
-        public OvertimeTypesController(IOvertimeTypeRepository overtimeTypesRepository)
+        public OvertimeTypesController(IOvertimeTypeBusiness overtimeTypesBusiness)
         {
-            _overtimeTypesRepository = overtimeTypesRepository;
+            _overtimeTypesBusiness = overtimeTypesBusiness;
         }
 
         [HttpPost("Get")]
         public async Task<List<OvertimeTypeDTO>> GetOvertimeTypes(BeanzCommonDTO common)
         {
-            var data = await _overtimeTypesRepository.GetOvertimeTypesAsync(common);
+            var data = await _overtimeTypesBusiness.GetOvertimeTypesAsync(common);
             return data;
         }
 
@@ -31,7 +32,7 @@ namespace Beanz.API.Areas.HummanResourceManagement.Masters
         {
             try
             {
-                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesRepository.SetOvertimeTypesAsync(common);
+                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesBusiness.SetOvertimeTypesAsync(common);
                 if (beanzResponseDTO.ErrorCode != "")
                     return BadRequest(beanzResponseDTO);
                 else
@@ -48,7 +49,7 @@ namespace Beanz.API.Areas.HummanResourceManagement.Masters
         {
             try
             {
-                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesRepository.PostOvertimeTypesAsync(common);
+                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesBusiness.PostOvertimeTypesAsync(common);
                 if (beanzResponseDTO.ErrorCode != "")
                     return BadRequest(beanzResponseDTO);
                 else
@@ -65,7 +66,7 @@ namespace Beanz.API.Areas.HummanResourceManagement.Masters
         {
             try
             {
-                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesRepository.DelOvertimeTypesAsync(common);
+                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesBusiness.DelOvertimeTypesAsync(common);
                 if (beanzResponseDTO.ErrorCode != "")
                     return BadRequest(beanzResponseDTO);
                 else
@@ -80,21 +81,21 @@ namespace Beanz.API.Areas.HummanResourceManagement.Masters
         [HttpPost("LookUp")]
         public async Task<List<BeanzlookupDTO>> LookUpOvertimeTypes(BeanzCommonDTO lookup)
         {
-            var data = await _overtimeTypesRepository.LookUpOvertimeTypesAsync(lookup);
+            var data = await _overtimeTypesBusiness.LookUpOvertimeTypesAsync(lookup);
             return data;
         }
 
         [HttpPost("GetInfo")]
         public async Task<OvertimeTypeViewModel> GetInfoOvertimeTypes(BeanzCommonDTO common)
         {
-            var data = await _overtimeTypesRepository.GetInfoOvertimeTypesAsync(common);
+            var data = await _overtimeTypesBusiness.GetInfoOvertimeTypesAsync(common);
             return data;
         }
 
         [HttpPost("Print")]
         public async Task<OvertimeTypeViewModel> PrintOvertimeTypes(BeanzCommonDTO common)
         {
-            var data = await _overtimeTypesRepository.PrintOvertimeTypesAsync(common);
+            var data = await _overtimeTypesBusiness.PrintOvertimeTypesAsync(common);
             return data;
         }
 
@@ -103,7 +104,7 @@ namespace Beanz.API.Areas.HummanResourceManagement.Masters
         {
             try
             {
-                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesRepository.ApproveOvertimeTypesAsync(common);
+                BeanzResponseDTO beanzResponseDTO = await _overtimeTypesBusiness.ApproveOvertimeTypesAsync(common);
                 if (beanzResponseDTO.ErrorCode != "")
                     return BadRequest(beanzResponseDTO);
                 else
