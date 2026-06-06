1252dd0b2d9f6121ddc374444fb0f46802f4cc9c
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-06 04:52:14 +0300

    Lovable: scaffold IRelationTypeBusiness.cs for RelationTypes

---
+++ Libraries/Beanz.IBusiness/Areas/HRM-HummanResourceManagement/Masters/IRelationTypeBusiness.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Beanz.DTOs.Areas.HummanResourceManagement.Masters;
+using Beanz.DTOs.Common;
+using Beanz.DTOs.BeanzRoutes;
+
+namespace Beanz.IBusiness.Areas.HummanResourceManagement.Masters
+{
+    public interface IRelationTypeBusiness
+    {
+        Task<List<RelationTypeDTO>> GetRelationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetRelationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostRelationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelRelationTypesAsync(BeanzCommonDTO common);
+        Task<RelationTypeViewModel> GetInfoRelationTypesAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpRelationTypesAsync(BeanzCommonDTO lookup);
+        Task<RelationTypeViewModel> PrintRelationTypesAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveRelationTypesAsync(BeanzCommonDTO common);
+    }
+}
