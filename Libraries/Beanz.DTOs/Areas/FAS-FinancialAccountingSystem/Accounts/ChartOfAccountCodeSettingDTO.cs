d8d57e0b3f33bcdfd8f0e6f18f0d523d2f96d7a4
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-12 06:21:13 +0300

    Lovable: scaffold ChartOfAccountCodeSettingDTO.cs for ChartOfAccountCodeSettings

---
+++ Libraries/Beanz.DTOs/Areas/FAS-FinancialAccountingSystem/Accounts/ChartOfAccountCodeSettingDTO.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.FinancialAccountingSystem.Accounts
+{
+    public class ChartOfAccountCodeSettingDTO
+    {
+        public int ChartOfAccountCodeSettingID { get; set; }
+        public string ChartOfAccountCodeSettingCode { get; set; }
+        public string ChartOfAccountCodeSettingName { get; set; }
+        public string ChartOfAccountCodeSettingAlias { get; set; }
+        public int LevelNo { get; set; }
+        public string LevelCodeFormat { get; set; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public bool IsUseParenLevelCode { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class ChartOfAccountCodeSettingViewModel
+    {
+        public List<ChartOfAccountCodeSettingDTO> ChartOfAccountCodeSettings { get; set; }
+        public ChartOfAccountCodeSettingDTO ChartOfAccountCodeSetting { get; set; }
+    }
+}
