92132adde4cd72b363619c958ce726881aeb25b7
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-12 06:21:10 +0300

    Lovable: scaffold IChartOfAccountCodeSettingRepository.cs for ChartOfAccountCodeSettings

---
+++ Libraries/Beanz.Core/Areas/FAS-FinancialAccountingSystem/Accounts/IChartOfAccountCodeSettingRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.FinancialAccountingSystem.Accounts;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.FinancialAccountingSystem.Accounts
+{
+    public interface IChartOfAccountCodeSettingRepository
+    {
+        Task<List<ChartOfAccountCodeSettingDTO>> GetChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpChartOfAccountCodeSettingsAsync(BeanzCommonDTO lookup);
+        Task<ChartOfAccountCodeSettingViewModel> GetInfoChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<ChartOfAccountCodeSettingViewModel> PrintChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveChartOfAccountCodeSettingsAsync(BeanzCommonDTO common);
+    }
+}
