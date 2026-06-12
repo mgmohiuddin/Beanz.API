6ecde7cdcb0d6ace07a13507867263a658085aef
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-12 06:08:42 +0300

    Lovable: scaffold ICostCenterRepository.cs for CostCenters

---
+++ Libraries/Beanz.Core/Areas/FAS-FinancialAccountingSystem/Masters/ICostCenterRepository.cs
@@ -0,0 +1,19 @@
+using Beanz.DTOs.Areas.FinancialAccountingSystem.Masters;
+using Beanz.DTOs.BeanzCommon;
+using Beanz.DTOs.BeanzRoutes;
+using Beanz.DTOs.Common;
+
+namespace Beanz.Core.Areas.FinancialAccountingSystem.Masters
+{
+    public interface ICostCenterRepository
+    {
+        Task<List<CostCenterDTO>> GetCostCentersAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> SetCostCentersAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> PostCostCentersAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> DelCostCentersAsync(BeanzCommonDTO common);
+        Task<List<BeanzlookupDTO>> LookUpCostCentersAsync(BeanzCommonDTO lookup);
+        Task<CostCenterViewModel> GetInfoCostCentersAsync(BeanzCommonDTO common);
+        Task<CostCenterViewModel> PrintCostCentersAsync(BeanzCommonDTO common);
+        Task<BeanzResponseDTO> ApproveCostCentersAsync(BeanzCommonDTO common);
+    }
+}
