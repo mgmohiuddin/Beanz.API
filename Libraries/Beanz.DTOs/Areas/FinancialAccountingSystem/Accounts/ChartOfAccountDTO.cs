e384611e08366abfc9c82c3d52456c557ee2b031
Mohammed Ghouse Mohiuddin <94669872+mgmohiuddin@users.noreply.github.com>
2026-06-13 18:06:55 +0300

    Lovable: scaffold ChartOfAccountDTO.cs for ChartOfAccounts

---
+++ Libraries/Beanz.DTOs/Areas/FinancialAccountingSystem/Accounts/ChartOfAccountDTO.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Beanz.DTOs.Areas.FinancialAccountingSystem.Accounts
+{
+    public class ChartOfAccountDTO
+    {
+        public int ChartOfAccountID { get; set; }
+        public int AccountTypeID { get; set; }
+        public string ChartOfAccountCode { get; set; }
+        public string ChartOfAccountName { get; set; }
+        public string ChartOfAccountAlias { get; set; }
+        public int ParentID { get; set; }
+        public int CompanyID { get; set; }
+        public int UserID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ApprovedDate { get; set; }
+        public int ApprovedBy { get; set; }
+        public DateTime PostedDate { get; set; }
+        public int PostedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public int DeletedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsPosted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+    public class ChartOfAccountViewModel
+    {
+        public List<ChartOfAccountDTO> ChartOfAccounts { get; set; }
+        public ChartOfAccountDTO ChartOfAccount { get; set; }
+    }
+}
